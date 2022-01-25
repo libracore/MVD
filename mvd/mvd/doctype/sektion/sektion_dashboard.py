8d847a3c08f1241c9274f77ff8108b484b77970b
Joelios <joel.mesmer@libracore.com>
2022-01-25 21:36:07 +0100

    neues feature: textkonserven

--- mvd/mvd/doctype/sektion/sektion_dashboard.py
+++ mvd/mvd/doctype/sektion/sektion_dashboard.py
@@ -9,7 +9,7 @@ def get_data():
         'transactions': [
             {
                 'label': _('Vorlagen'),
-                'items': ['MV Korrespondenz Vorlage', 'Terminkategorie']
+                'items': ['MV Korrespondenz Vorlage', 'Terminkategorie', 'Textkonserven']
             }
         ]
     }
