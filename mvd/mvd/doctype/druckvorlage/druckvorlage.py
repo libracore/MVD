5d0b47246997fd56981c79c6f1f8549b18209edd
Joelios <joel.mesmer@libracore.com>
2022-02-01 14:44:11 +0100

    druckvorlage

--- mvd/mvd/doctype/druckvorlage/druckvorlage.py
+++ mvd/mvd/doctype/druckvorlage/druckvorlage.py
@@ -14,43 +14,74 @@ class Druckvorlage(Document):
     
     def validiere_inhalt(self):
         if self.dokument in ('Beitritt mit EZ', 'Interessent*Innenbrief mit EZ'):
-            if self.anzahl_seiten == '1':
-                frappe.throw("Es müssen mindestens 2 Seiten aktiviert werden (Mitgliedschaftsrechnung & HV Rechnung)")
-            
-            if self.anzahl_seiten == '2':
-                if self.seite_1_qrr == 'Keiner' or self.seite_2_qrr == 'Keiner':
-                    frappe.throw("Bei 2 Seiten muss sowohl der Mitgliedschafts QRR wie auch der HV QRR ausgewählt werden.")
-                if self.seite_1_qrr == self.seite_2_qrr:
-                    frappe.throw("Es kann nicht derselbe QRR Typ für Seite 1 wie auch Seite 2 gewählt werden.")
-            
-            if self.anzahl_seiten == '3':
-                if self.seite_2_qrr == self.seite_3_qrr:
-                    frappe.throw("Es kann nicht derselbe QRR Typ für Seite 2 wie auch Seite 3 gewählt werden.")
-            
-            mitgliedschaft = False
-            hv = False
-            if self.anzahl_seiten == '2':
-                if self.seite_1_qrr == 'Mitgliedschaft':
-                    mitgliedschaft = True
-                if self.seite_1_qrr == 'HV':
-                    hv = True
-                if self.seite_2_qrr == 'Mitgliedschaft':
-                    mitgliedschaft = True
-                if self.seite_2_qrr == 'HV':
-                    hv = True
-            else:
-                if self.seite_2_qrr == 'Mitgliedschaft':
-                    mitgliedschaft = True
-                if self.seite_2_qrr == 'HV':
-                    hv = True
-                if self.seite_3_qrr == 'Mitgliedschaft':
-                    mitgliedschaft = True
-                if self.seite_3_qrr == 'HV':
-                    hv = True
-            if not mitgliedschaft:
-                frappe.throw("Bitte wählen Sie als QRR Typ exakt einmal Mitgliedschaft aus.")
-            if not hv:
-                frappe.throw("Bitte wählen Sie als QRR Typ exakt einmal HV aus.")
+            if self.mitgliedtyp_c == 'Privat':
+                if self.anzahl_seiten == '1':
+                    frappe.throw("Es müssen mindestens 2 Seiten aktiviert werden (Mitgliedschaftsrechnung & HV Rechnung)")
+                
+                if self.anzahl_seiten == '2':
+                    if self.seite_1_qrr == 'Keiner' or self.seite_2_qrr == 'Keiner':
+                        frappe.throw("Bei 2 Seiten muss sowohl der Mitgliedschafts QRR wie auch der HV QRR ausgewählt werden.")
+                    if self.seite_1_qrr == self.seite_2_qrr:
+                        frappe.throw("Es kann nicht derselbe QRR Typ für Seite 1 wie auch Seite 2 gewählt werden.")
+                
+                if self.anzahl_seiten == '3':
+                    if self.seite_2_qrr == self.seite_3_qrr:
+                        frappe.throw("Es kann nicht derselbe QRR Typ für Seite 2 wie auch Seite 3 gewählt werden.")
+                
+                mitgliedschaft = False
+                hv = False
+                if self.anzahl_seiten == '2':
+                    if self.seite_1_qrr == 'Mitgliedschaft':
+                        mitgliedschaft = True
+                    if self.seite_1_qrr == 'HV':
+                        hv = True
+                    if self.seite_2_qrr == 'Mitgliedschaft':
+                        mitgliedschaft = True
+                    if self.seite_2_qrr == 'HV':
+                        hv = True
+                else:
+                    if self.seite_2_qrr == 'Mitgliedschaft':
+                        mitgliedschaft = True
+                    if self.seite_2_qrr == 'HV':
+                        hv = True
+                    if self.seite_3_qrr == 'Mitgliedschaft':
+                        mitgliedschaft = True
+                    if self.seite_3_qrr == 'HV':
+                        hv = True
+                if not mitgliedschaft:
+                    frappe.throw("Bitte wählen Sie als QRR Typ exakt einmal Mitgliedschaft aus.")
+                if not hv:
+                    frappe.throw("Bitte wählen Sie als QRR Typ exakt einmal HV aus.")
+            elif self.mitgliedtyp_c == 'Geschäft':
+                mitgliedschaft = 0
+                hv = 0
+                if self.anzahl_seiten == '1':
+                    if self.seite_1_qrr == 'Mitgliedschaft':
+                        mitgliedschaft += 1
+                    if self.seite_1_qrr == 'HV':
+                        hv += 1
+                elif self.anzahl_seiten == '2':
+                    if self.seite_1_qrr == 'Mitgliedschaft':
+                        mitgliedschaft += 1
+                    if self.seite_1_qrr == 'HV':
+                        hv += 1
+                    if self.seite_2_qrr == 'Mitgliedschaft':
+                        mitgliedschaft += 1
+                    if self.seite_2_qrr == 'HV':
+                        hv += 1
+                else:
+                    if self.seite_2_qrr == 'Mitgliedschaft':
+                        mitgliedschaft += 1
+                    if self.seite_2_qrr == 'HV':
+                        hv += 1
+                    if self.seite_3_qrr == 'Mitgliedschaft':
+                        mitgliedschaft += 1
+                    if self.seite_3_qrr == 'HV':
+                        hv += 1
+                if mitgliedschaft != 1:
+                    frappe.throw("Bitte wählen Sie als QRR Typ exakt einmal Mitgliedschaft aus.")
+                if hv > 0:
+                    frappe.throw("Der QRR Typ HV darf in Kombination mit Mitgliedtyp Geschäft nicht gewählt werden.")
                 
         elif self.dokument == 'Begrüssung mit Ausweis':
             if self.doppelseitiger_druck != 1:
