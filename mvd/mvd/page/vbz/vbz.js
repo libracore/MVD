abc4d98f0e8c61a2fa09cc5cc26c7a7342a6d5bb
Joelios <joel.mesmer@libracore.com>
2022-02-05 21:46:56 +0100

    vbz

--- mvd/mvd/page/vbz/vbz.js
+++ mvd/mvd/page/vbz/vbz.js
@@ -25,9 +25,9 @@ frappe.vbz = {
                 if (r.message) {
                     page.add_view('overview', frappe.render_template("overview", eval(r.message)))
                     page.add_view('validierung', frappe.render_template("validierung", eval(r.message.validierung)))
-                    page.add_view('massenlauf', frappe.render_template("massenlauf", {}))
+                    page.add_view('massenlauf', frappe.render_template("massenlauf", {'kuendigung': eval(r.message.kuendigung_massenlauf)}))
                     page.add_view('adresspflege', frappe.render_template("adresspflege", {}))
-                    frappe.vbz.add_click_handlers();
+                    frappe.vbz.add_click_handlers(eval(r.message));
                 }
             }
         });
@@ -50,7 +50,7 @@ frappe.vbz = {
         $("#adresspflege_second").off("click");
         $("#goto_klassisch").off("click");
     },
-    add_click_handlers: function() {
+    add_click_handlers: function(open_datas) {
         frappe.vbz.remove_click_handlers();
         $("#camt").click(function(){
             var default_sektion = frappe.vbz.get_default_sektion();
@@ -107,6 +107,45 @@ frappe.vbz = {
         });
         $("#neuanlage").click(function(){
             frappe.set_route("mvd-suchmaske");
+        });
+        
+        // validierungen
+        $("#online_beitritte").click(function(){
+            frappe.route_options = {"name": ['in', open_datas.validierung.online_beitritt.names]}
+            frappe.set_route("List", "Mitgliedschaft");
+        });
+        $("#online_anmeldungen").click(function(){
+            frappe.route_options = {"name": ['in', open_datas.validierung.online_anmeldung.names]}
+            frappe.set_route("List", "Mitgliedschaft");
+        });
+        $("#online_kuendigungen").click(function(){
+            frappe.route_options = {"name": ['in', open_datas.validierung.online_kuendigung.names]}
+            frappe.set_route("List", "Mitgliedschaft");
+        });
+        $("#zuzuege").click(function(){
+            frappe.route_options = {"name": ['in', open_datas.validierung.zuzug.names]}
+            frappe.set_route("List", "Mitgliedschaft");
+        });
+        
+        // massenlauf
+        $("#keundigung_qty").click(function(){
+            frappe.route_options = {"name": ['in', open_datas.kuendigung_massenlauf.names]}
+            frappe.set_route("List", "Mitgliedschaft");
+        });
+        $("#kuendigung_print").click(function(){
+            frappe.msgprint("Das muss noch programmiert werden!");
+        });
+        $("#zuzug_qty").click(function(){
+            frappe.msgprint("Das muss noch programmiert werden!");
+        });
+        $("#zuzug_print").click(function(){
+            frappe.msgprint("Das muss noch programmiert werden!");
+        });
+        $("#begruessung_qty").click(function(){
+            frappe.msgprint("Das muss noch programmiert werden!");
+        });
+        $("#begruessung_print").click(function(){
+            frappe.msgprint("Das muss noch programmiert werden!");
         });
     },
     get_default_sektion: function() {
--- mvd/mvd/page/vbz/vbz.py
+++ mvd/mvd/page/vbz/vbz.py
@@ -23,43 +23,136 @@ def get_open_data(sektion=None):
                                             COUNT(`name`) AS `qty`
                                         FROM `tabArbeits Backlog`
                                         WHERE `status` = 'Open'
-                                        AND `typ` IN ('Daten Validieren', 'Beitritts Validierung')
-                                        {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
-    validierung_allgemein = frappe.db.sql("""SELECT
-                                            COUNT(`name`) AS `qty`
-                                        FROM `tabArbeits Backlog`
-                                        WHERE `status` = 'Open'
                                         AND `typ` = 'Daten Validieren'
                                         {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
-    validierung_beitritt = frappe.db.sql("""SELECT
+    v_online_beitritt_qty = frappe.db.sql("""SELECT
                                             COUNT(`name`) AS `qty`
-                                        FROM `tabArbeits Backlog`
-                                        WHERE `status` = 'Open'
-                                        AND `typ` = 'Beitritts Validierung'
+                                        FROM `tabMitgliedschaft`
+                                        WHERE `validierung_notwendig` = 1
+                                        AND `status_c` = 'Online-Beitritt'
                                         {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
+    _v_online_beitritt = frappe.db.sql("""SELECT
+                                            `name`
+                                        FROM `tabMitgliedschaft`
+                                        WHERE `validierung_notwendig` = 1
+                                        AND `status_c` = 'Online-Beitritt'
+                                        {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)
+    v_online_beitritt = 'x'
+    for online_beitritt in _v_online_beitritt:
+        v_online_beitritt += ',' + online_beitritt.name
+    if len(_v_online_beitritt) > 0:
+        v_online_beitritt = v_online_beitritt.replace("x,", "")
+    else:
+        v_online_beitritt = ''
     
-    # Kündigungen
-    kuendigung_qty = frappe.db.sql("""SELECT COUNT(`name`) AS `qty` FROM `tabArbeits Backlog` WHERE `status` = 'Open' AND `typ` = 'Kündigung verarbeiten'{sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
-    alle_kuendigungen = frappe.db.sql("""SELECT
-                                            `tabArbeits Backlog`.`mv_mitgliedschaft`,
-                                            DATE_FORMAT(`tabMitgliedschaft`.`kuendigung`, '%d.%m.%Y') AS `datum`
-                                        FROM `tabArbeits Backlog`
-                                        LEFT JOIN `tabMitgliedschaft` ON `tabArbeits Backlog`.`mv_mitgliedschaft` = `tabMitgliedschaft`.`name`
-                                        WHERE `tabArbeits Backlog`.`status` = 'Open'
-                                        AND `tabArbeits Backlog`.`typ` = 'Kündigung verarbeiten'
-                                        {sektion_filter}""".format(sektion_filter=sektion_filter.replace("AND `sektion_id`", "AND `tabArbeits Backlog`.`sektion_id`")), as_dict=True)
+    v_online_anmeldung_qty = frappe.db.sql("""SELECT
+                                            COUNT(`name`) AS `qty`
+                                        FROM `tabMitgliedschaft`
+                                        WHERE `validierung_notwendig` = 1
+                                        AND `status_c` = 'Online-Anmeldung'
+                                        {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
+    _v_online_anmeldung = frappe.db.sql("""SELECT
+                                            `name`
+                                        FROM `tabMitgliedschaft`
+                                        WHERE `validierung_notwendig` = 1
+                                        AND `status_c` = 'Online-Anmeldung'
+                                        {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)
+    v_online_anmeldung = 'x'
+    for online_anmeldung in _v_online_anmeldung:
+        v_online_anmeldung += ',' + online_anmeldung.name
+    if len(_v_online_beitritt) > 0:
+        v_online_anmeldung = v_online_anmeldung.replace("x,", "")
+    else:
+        v_online_anmeldung = ''
+    
+    v_online_kuendigung_qty = frappe.db.sql("""SELECT
+                                            COUNT(`name`) AS `qty`
+                                        FROM `tabMitgliedschaft`
+                                        WHERE `validierung_notwendig` = 1
+                                        AND `status_c` = 'Online-Kündigung'
+                                        {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
+    _v_online_kuendigung = frappe.db.sql("""SELECT
+                                            `name`
+                                        FROM `tabMitgliedschaft`
+                                        WHERE `validierung_notwendig` = 1
+                                        AND `status_c` = 'Online-Kündigung'
+                                        {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)
+    v_online_kuendigung = 'x'
+    for online_kuendigung in _v_online_kuendigung:
+        v_online_kuendigung += ',' + online_kuendigung.name
+    if len(_v_online_kuendigung) > 0:
+        v_online_kuendigung = v_online_kuendigung.replace("x,", "")
+    else:
+        v_online_kuendigung = ''
+    
+    v_zuzug_qty = frappe.db.sql("""SELECT
+                                        COUNT(`name`) AS `qty`
+                                    FROM `tabMitgliedschaft`
+                                    WHERE `validierung_notwendig` = 1
+                                    AND `status_c` = 'Zuzug'
+                                    {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
+    _v_zuzug = frappe.db.sql("""SELECT
+                                        `name`
+                                    FROM `tabMitgliedschaft`
+                                    WHERE `validierung_notwendig` = 1
+                                    AND `status_c` = 'Zuzug'
+                                    {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)
+    v_zuzug = 'x'
+    for zuzug in _v_zuzug:
+        v_zuzug += ',' + zuzug.name
+    if len(_v_zuzug) > 0:
+        v_zuzug = v_zuzug.replace("x,", "")
+    else:
+        v_zuzug = ''
+    
+    # kündigung massenlauf
+    kuendigung_qty = frappe.db.sql("""SELECT
+                                        COUNT(`name`) AS `qty`
+                                    FROM `tabMitgliedschaft`
+                                    WHERE `kuendigung_verarbeiten` = 1
+                                    {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)[0].qty
+    _kuendigung = frappe.db.sql("""SELECT
+                                        `name`
+                                    FROM `tabMitgliedschaft`
+                                    WHERE `kuendigung_verarbeiten` = 1
+                                    {sektion_filter}""".format(sektion_filter=sektion_filter), as_dict=True)
+    kuendigung = 'x'
+    for k in _kuendigung:
+        kuendigung += ',' + k.name
+    if len(_kuendigung) > 0:
+        kuendigung = kuendigung.replace("x,", "")
+    else:
+        kuendigung = ''
+    
+    # massenlauf total
+    massenlauf_total = kuendigung_qty
     
     return {
         'arbeits_backlog': {
             'qty': abl_qty
         },
+        'massenlauf_total': massenlauf_total,
         'validierung': {
             'qty': validierung_total,
-            'allgemein': validierung_allgemein,
-            'beitritt': validierung_beitritt
+            'online_beitritt': {
+                'qty': v_online_beitritt_qty,
+                'names': v_online_beitritt
+            },
+            'online_anmeldung': {
+                'qty': v_online_anmeldung_qty,
+                'names': v_online_anmeldung
+            },
+            'online_kuendigung': {
+                'qty': v_online_kuendigung_qty,
+                'names': v_online_kuendigung
+            },
+            'zuzug': {
+                'qty': v_zuzug_qty,
+                'names': v_zuzug
+            }
         },
-        'kuendigung': {
+        'kuendigung_massenlauf': {
             'qty': kuendigung_qty,
-            'alle_kuendigungen': alle_kuendigungen
+            'names': kuendigung
         }
     }
