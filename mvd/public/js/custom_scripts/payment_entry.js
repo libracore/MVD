d0ea63e9e7fce8a3a4b016e4f18288c5dbef8390
Joelios <joel.mesmer@libracore.com>
2022-02-15 18:14:40 +0100

    camt

--- mvd/public/js/custom_scripts/payment_entry.js
+++ mvd/public/js/custom_scripts/payment_entry.js
@@ -5,6 +5,9 @@ frappe.ui.form.on('Payment Entry', {
     refresh: function(frm) {
         if (!frm.doc.__islocal) {
             if (cur_frm.doc.unallocated_amount > 0) {
+                frm.add_custom_button(__("Mitgliedschafts-Korrespondenz erstellen"), function() {
+                    frappe.msgprint("Muss noch programmiert werden!");
+                });
                 if ((cur_frm.doc.paid_amount / cur_frm.doc.unallocated_amount) == 2) {
                     frm.add_custom_button(__("Mit Folgejahr-Mitgliedschaft ausgleichen"), function() {
                         frappe.msgprint("Muss noch programmiert werden!");
