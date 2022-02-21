58cc2b7c5ed6c566781232f44097b418a43af3db
Joelios <joel.mesmer@libracore.com>
2022-02-21 18:50:02 +0100

    permission fix

--- mvd/mvd/utils/notifications.py
+++ mvd/mvd/utils/notifications.py
@@ -9,19 +9,3 @@ def get_notification_config():
             "Arbeits Backlog": {"status": 'Open'}
         }
     }
-
-def get_todo_permission_query_conditions(user):
-    allowed_sektionen = frappe.get_list('Sektion', fields=['virtueller_user'])
-    
-    todo_users = [user or frappe.session.user]
-    for virt_user in allowed_sektionen:
-        todo_users.append(virt_user['virtueller_user'])
-
-    if "System Manager" in frappe.get_roles(user):
-        return None
-    else:
-        if allowed_sektionen:
-            return """(`tabToDo`.owner IN {user} or `tabToDo`.assigned_by IN {user})"""\
-                .format(user=tuple(todo_users))
-        else:
-            None
