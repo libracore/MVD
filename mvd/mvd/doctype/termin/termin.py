3a68c0638cfec3270d0f3f8f8cf1a1193108939d
Joelios <joel.mesmer@libracore.com>
2022-02-22 13:15:49 +0100

    hotfix: termin permission

--- mvd/mvd/doctype/termin/termin.py
+++ mvd/mvd/doctype/termin/termin.py
@@ -3,8 +3,11 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Termin(Document):
-	pass
+    def validate(self):
+        if not self.mv_mitgliedschaft:
+            frappe.throw("Termine müssen immer einer Mitgliedschaft zugewiesen sein")
+        self.sektion_id = frappe.get_value("Mitgliedschaft", self.mv_mitgliedschaft, "sektion_id")
