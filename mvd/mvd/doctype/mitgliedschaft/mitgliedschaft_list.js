3f873c78babbf75e42df48c55184a640fa28cfdf
Joelios <joel.mesmer@libracore.com>
2022-02-23 12:07:13 +0100

    Rollen: MV_RB, MV_MA, MV_RW und MV_GL

--- mvd/mvd/doctype/mitgliedschaft/mitgliedschaft_list.js
+++ mvd/mvd/doctype/mitgliedschaft/mitgliedschaft_list.js
@@ -6,11 +6,7 @@ frappe.listview_settings['Mitgliedschaft'] = {
         listview.page.add_action_item(__("Erstelle Serienbrief"), function() {
                 var selected = listview.get_checked_items();
                 if (selected.length > 0) {
-                    if (!frappe.user.has_role("MV Sektionsmitarbeiter*in RO")&&!frappe.user.has_role("RB (Rechtsberater*in)")) {
-                        create_serienbrief(selected);
-                    } else {
-                        frappe.msgprint("Sie haben eine Read-Only Rolle und sind für zur Ausführung dieser Aktion nicht berechtigt.");
-                    }
+                    create_serienbrief(selected);
                 } else {
                     frappe.msgprint("Bitte markieren Sie zuerst die gewünschten Mitgliedschaften");
                 }
@@ -19,46 +15,50 @@ frappe.listview_settings['Mitgliedschaft'] = {
 };
 
 function create_serienbrief(mitgliedschaften) {
-    frappe.call({
-        method: "mvd.mvd.doctype.druckvorlage.druckvorlage.get_druckvorlagen",
-        args:{
-                'sektion': mitgliedschaften[0].name,
-                'serienbrief': 1
-        },
-        async: false,
-        callback: function(res)
-        {
-            var druckvorlagen = res.message;
-            frappe.prompt([
-                {'fieldname': 'titel', 'fieldtype': 'Data', 'label': 'Titel', 'reqd': 1},
-                {'fieldname': 'druckvorlage', 'fieldtype': 'Link', 'label': 'Druckvorlage', 'reqd': 1, 'options': 'Druckvorlage', 
-                    'get_query': function() {
-                        return { 'filters': { 'name': ['in', eval(druckvorlagen)] } };
-                    }
-                }
-            ],
-            function(values){
-                frappe.call({
-                    method: "mvd.mvd.doctype.mitgliedschaft.mitgliedschaft.create_korrespondenz_massenlauf",
-                    args:{
-                            'mitgliedschaften': mitgliedschaften,
-                            'druckvorlage': values.druckvorlage,
-                            'titel': values.titel
-                    },
-                    freeze: true,
-                    freeze_message: 'Erstelle Serienbrief...',
-                    callback: function(r)
-                    {
-                        frappe.msgprint("Die Serienbriefe wurden als Korrespondenzen erzeugt und können einzeln via Mitgliedschaft oder als Sammel-PDF via Verarbeitungszentrale gedruckt werden.");
-                    }
-                });
-                
+    if (frappe.user.has_role("MV_MA")) {
+        frappe.call({
+            method: "mvd.mvd.doctype.druckvorlage.druckvorlage.get_druckvorlagen",
+            args:{
+                    'sektion': mitgliedschaften[0].name,
+                    'serienbrief': 1
             },
-            'Serienbrief Erstellung',
-            'Erstellen'
-            )
-        }
-    });
+            async: false,
+            callback: function(res)
+            {
+                var druckvorlagen = res.message;
+                frappe.prompt([
+                    {'fieldname': 'titel', 'fieldtype': 'Data', 'label': 'Titel', 'reqd': 1},
+                    {'fieldname': 'druckvorlage', 'fieldtype': 'Link', 'label': 'Druckvorlage', 'reqd': 1, 'options': 'Druckvorlage', 
+                        'get_query': function() {
+                            return { 'filters': { 'name': ['in', eval(druckvorlagen)] } };
+                        }
+                    }
+                ],
+                function(values){
+                    frappe.call({
+                        method: "mvd.mvd.doctype.mitgliedschaft.mitgliedschaft.create_korrespondenz_massenlauf",
+                        args:{
+                                'mitgliedschaften': mitgliedschaften,
+                                'druckvorlage': values.druckvorlage,
+                                'titel': values.titel
+                        },
+                        freeze: true,
+                        freeze_message: 'Erstelle Serienbrief...',
+                        callback: function(r)
+                        {
+                            frappe.msgprint("Die Serienbriefe wurden als Korrespondenzen erzeugt und können einzeln via Mitgliedschaft oder als Sammel-PDF via Verarbeitungszentrale gedruckt werden.");
+                        }
+                    });
+                    
+                },
+                'Serienbrief Erstellung',
+                'Erstellen'
+                )
+            }
+        });
+    } else {
+        frappe.msgprint("Sie haben keine Berechtigung zur Ausführung dieser Aktion.");
+    }
 }
 
 // obsolet vbz
