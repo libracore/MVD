a3a0bf5f07006588bd7d774f691816fa1c9a6a8d
Joelios <joel.mesmer@libracore.com>
2022-06-17 11:46:12 +0200

    v1.76.67; Terminkategorie deaktivierbar (632)

--- mvd/mvd/doctype/termin/termin.js
+++ mvd/mvd/doctype/termin/termin.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Termin', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        cur_frm.fields_dict['kategorie'].get_query = function(doc) {
+             return {
+                 filters: {
+                     "disabled": ['!=', 1],
+                     "sektion_id": cur_frm.doc.sektion_id
+                 }
+             }
+        }
+    }
 });
