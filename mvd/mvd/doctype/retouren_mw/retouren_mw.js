da7dae3220ed15b6ce88aff5b6a4e577a3e034b8
Joelios <joel.mesmer@libracore.com>
2022-06-23 10:23:52 +0200

    adresspflege

--- mvd/mvd/doctype/retouren_mw/retouren_mw.js
+++ mvd/mvd/doctype/retouren_mw/retouren_mw.js
@@ -2,7 +2,33 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Retouren MW', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        cur_frm.page.add_action_icon(__("fa fa-envelope-o"), function() {
+            send_mail(frm);
+        });
+    }
 });
+
+function send_mail(frm) {
+    frappe.call({
+        method: "mvd.mvd.doctype.retouren_mw.retouren_mw.get_mail_data",
+        args:{
+                'mitgliedschaft': cur_frm.doc.mv_mitgliedschaft,
+                'retoure': cur_frm.doc.name,
+                'grund_bezeichnung': cur_frm.doc.grund_bezeichnung || 'Keine Angaben'
+        },
+        callback: function(r)
+        {
+            if (r.message) {
+                var mail_data = r.message;
+                var email = mail_data.email;
+                var cc = mail_data.cc;
+                var subject = mail_data.subject;
+                var email_body = mail_data.email_body;
+                document.location = "mailto:"+email+"?cc="+cc+"&subject="+subject+"&body="+email_body;
+            } else {
+                frappe.msgprint("Das Mitglied besitzt keine E-Mail Adresse");
+            }
+        }
+    });
+};
--- mvd/mvd/doctype/retouren_mw/retouren_mw.py
+++ mvd/mvd/doctype/retouren_mw/retouren_mw.py
@@ -35,19 +35,23 @@ class RetourenMW(Document):
                 mitgliedschaft.m_w_retouren_offen = 0
                 mitgliedschaft.m_w_retouren_in_bearbeitung = 0
                 mitgliedschaft.save()
-    def onload(self):
-        if int(self.onload_frei) == 1:
-            mitgliedschaft = frappe.get_doc("Mitgliedschaft", self.mv_mitgliedschaft)
-            adresse = frappe.get_doc("Address", mitgliedschaft.adresse_mitglied)
-            datum_adressexport = frappe.db.sql("""SELECT `datum_adressexport` FROM `tabMW` WHERE `laufnummer` = '{retoure_mw_sequence_number}' LIMIT 1""".format(retoure_mw_sequence_number=self.retoure_mw_sequence_number), as_dict=True)[0].datum_adressexport
-            if getdate(adresse.creation) > getdate(datum_adressexport):
-                self.adresse_geaendert = 1
-            else: self.adresse_geaendert = 0
+        
+        adresse = frappe.db.sql("""SELECT `adresse_mitglied` FROM `tabMitgliedschaft` WHERE `name` = '{mitgliedschaft}'""".format(mitgliedschaft=self.mv_mitgliedschaft), as_dict=True)[0].adresse_mitglied
+        adresse = frappe.get_doc("Address", adresse)
+        datum_adressexport = frappe.db.sql("""SELECT `datum_adressexport` FROM `tabMW` WHERE `laufnummer` = '{retoure_mw_sequence_number}' LIMIT 1""".format(retoure_mw_sequence_number=self.retoure_mw_sequence_number), as_dict=True)[0].datum_adressexport
+        if getdate(adresse.modified) > getdate(datum_adressexport):
+            self.adresse_geaendert = 1
 
 def create_post_retouren(data):
     try:
         ausgabe_kurz = frappe.db.sql("""SELECT `ausgabe_kurz` FROM `tabMW` WHERE `laufnummer` = '{retoure_mw_sequence_number}' LIMIT 1""".format(retoure_mw_sequence_number=data['retoureMuWSequenceNumber']), as_dict=True)[0].ausgabe_kurz
         mitgliedschaft = frappe.get_doc("Mitgliedschaft", data['mitgliedId'])
+        adresse = frappe.get_doc("Address", mitgliedschaft.adresse_mitglied)
+        datum_adressexport = frappe.db.sql("""SELECT `datum_adressexport` FROM `tabMW` WHERE `laufnummer` = '{retoure_mw_sequence_number}' LIMIT 1""".format(retoure_mw_sequence_number=self.retoure_mw_sequence_number), as_dict=True)[0].datum_adressexport
+        if getdate(adresse.modified) > getdate(datum_adressexport):
+            adresse_geaendert = 1
+        else:
+            adresse_geaendert = 0
         post_retoure = frappe.get_doc({
             'doctype': 'Retouren MW',
             'mv_mitgliedschaft': data['mitgliedId'],
@@ -61,7 +65,7 @@ def create_post_retouren(data):
             'retoure_dmc': data['retoureDMC'],
             'retoureSendungsbild': data['retoureSendungsbild'],
             'datum_erfasst_post': data['datumErfasstPost'],
-            'onload_frei': 1
+            'adresse_geaendert': adresse_geaendert
         })
         
         post_retoure.insert(ignore_permissions=True)
@@ -77,3 +81,28 @@ def close_open_retouren(mitgliedschaft):
         for retoure in retouren:
             update = frappe.db.sql("""UPDATE `tabRetouren MW` SET `status` = 'Abgeschlossen' WHERE `name` = '{retoure}'""".format(retoure=retoure.name), as_list=True)
         frappe.db.commit()
+
+def check_dates(adresse, event):
+    mitgliedschaften = frappe.db.sql("""SELECT `name` FROM `tabMitgliedschaft` WHERE `adresse_mitglied` = '{adresse}' LIMIT 1""".format(adresse=adresse.name), as_dict=True)
+    if len(mitgliedschaften) > 0:
+        mitgliedschaft = mitgliedschaften[0].name
+        retouren = frappe.db.sql("""SELECT `name` FROM `tabRetouren MW` WHERE `mv_mitgliedschaft` = '{mitgliedschaft}' AND `status` != 'Abgeschlossen'""".format(mitgliedschaft=mitgliedschaft), as_dict=True)
+        for retoure in retouren:
+            retoure = frappe.get_doc("Retouren MW", retoure.name)
+            datum_adressexport = frappe.db.sql("""SELECT `datum_adressexport` FROM `tabMW` WHERE `laufnummer` = '{retoure_mw_sequence_number}' LIMIT 1""".format(retoure_mw_sequence_number=retoure.retoure_mw_sequence_number), as_dict=True)[0].datum_adressexport
+            if getdate(adresse.modified) > getdate(datum_adressexport):
+                retoure.adresse_geaendert = 1
+                retoure.save(ignore_permissions=True)
+
+@frappe.whitelist()
+def get_mail_data(mitgliedschaft, retoure, grund_bezeichnung):
+    mitgliedschaft = frappe.get_doc("Mitgliedschaft", mitgliedschaft)
+    if mitgliedschaft.e_mail_1:
+        return {
+            'email': mitgliedschaft.e_mail_1,
+            'cc': 'mv+Mitgliedschaft+{0}@libracore.io,mv+Retouren%20MW+{1}@libracore.io'.format(mitgliedschaft.name, retoure),
+            'subject': 'Ihre Mitgliedschaft: Neue Adresse?',
+            'email_body': '{0}%0d%0a%0d%0aDie Post konnte Ihnen unsere Verbandszeitschrift M+W nicht zustellen. Sie wurde retourniert mit dem Vermerk «{1}».%0d%0aBitte teilen Sie uns Ihre aktuelle Wohnadresse mit, damit wir Ihnen auch weiterhin unsere Post zustellen können.%0d%0a%0d%0aSie sind in unserer Mitgliederdatei unter nachfolgender Adresse geführt:%0d%0a{2}%0d%0a%0d%0aFreundliche Grüsse'.format(mitgliedschaft.briefanrede, grund_bezeichnung, mitgliedschaft.adressblock.replace("\n", "%0d%0a"))
+        }
+    else:
+        return None
