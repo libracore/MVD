981a7f771819005c295743b614b4bd29a50370b7
Joelios <joel.mesmer@libracore.com>
2022-09-28 16:34:53 +0200

    v3.5.6; Default language = de in Kunden

--- mvd/mvd/doctype/kunden/kunden.js
+++ mvd/mvd/doctype/kunden/kunden.js
@@ -2,6 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Kunden', {
+    onload: function(frm) {
+        if (frm.doc.__islocal) {
+            setTimeout(function(){ cur_frm.set_value("language", "de"); }, 500);
+        }
+    },
     refresh: function(frm) {
         // buttons
         if (!frm.doc.__islocal) {
