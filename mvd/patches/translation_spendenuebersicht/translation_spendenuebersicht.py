1691fd26001a69fbeade7edb87c1c073edff169a
Joelios <joel.mesmer@libracore.com>
2022-11-11 10:43:58 +0100

    v5.12.8

---
+++ mvd/patches/translation_spendenuebersicht/translation_spendenuebersicht.py
@@ -0,0 +1,21 @@
+import frappe
+from frappe import _
+from mvd.mvd.doctype.mitgliedschaft.mitgliedschaft import get_anredekonvention
+
+def execute():
+    print("Erstelle Übersaetzungen")
+    new_translation_de = frappe.get_doc({
+        "doctype": "Translation",
+        "language": "de",
+        "source_name": "Spendenuebersicht",
+        "target_name": "Spendenübersicht"
+    })
+    new_translation_de.insert()
+    new_translation_en = frappe.get_doc({
+        "doctype": "Translation",
+        "language": "en",
+        "source_name": "Spendenuebersicht",
+        "target_name": "Spendenübersicht"
+    })
+    new_translation_en.insert()
+    return
