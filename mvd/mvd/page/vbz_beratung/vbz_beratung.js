247f6f32ef6933284f935cfdec5ff02571b50f10
Joelios <joel.mesmer@libracore.com>
2023-07-03 10:31:02 +0200

    v8.11.4: Anpassung Filter S6

--- mvd/mvd/page/vbz_beratung/vbz_beratung.js
+++ mvd/mvd/page/vbz_beratung/vbz_beratung.js
@@ -64,7 +64,7 @@ frappe.vbz_beratung = {
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s6").click(function(){
-            frappe.route_options = {"status": ["not in", ["Rückfragen", "Rückfrage: Termin vereinbaren", "Eingang"]], "ungelesen": 1}
+            frappe.route_options = {"status": ["not in", ["Rückfragen", "Rückfrage: Termin vereinbaren", "Eingang", "Open"]], "ungelesen": 1}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s7").click(function(){
