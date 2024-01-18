93778199457b6af303073445971c8917b7c06c40
joelios <joel.mesmer@libracore.com>
2024-01-18 20:13:19 +0100

    v11.33.0

--- mvd/mvd/page/vbz_beratung/vbz_beratung.js
+++ mvd/mvd/page/vbz_beratung/vbz_beratung.js
@@ -40,80 +40,80 @@ frappe.vbz_beratung = {
         //~ frappe.vbz_beratung.remove_click_handlers();
         
         $("#s").click(function(){
-            frappe.route_options = {"status": 'Eingang'}
+            frappe.route_options = {"status": 'Eingang', "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s1").click(function(){
-            frappe.route_options = {"status": 'Eingang', "mv_mitgliedschaft": ['is', 'not set']}
+            frappe.route_options = {"status": 'Eingang', "mv_mitgliedschaft": ['is', 'not set'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s2").click(function(){
-            frappe.route_options = {"status": 'Eingang', "beratungskategorie": ['in', ['202 - MZ-Erhöhung', '300 - Nebenkosten']]}
+            frappe.route_options = {"status": 'Eingang', "beratungskategorie": ['in', ['202 - MZ-Erhöhung', '300 - Nebenkosten']], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s3").click(function(){
-            frappe.route_options = {"status": 'Rückfrage: Termin vereinbaren'}
+            frappe.route_options = {"status": 'Rückfrage: Termin vereinbaren', "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s4").click(function(){
-            frappe.route_options = {"status": "Rückfragen", "kontaktperson": ['is', 'not set']}
+            frappe.route_options = {"status": "Rückfragen", "kontaktperson": ['is', 'not set'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s5").click(function(){
-            frappe.route_options = {"status": "Rückfragen", "kontaktperson": ['is', 'not set'], "ungelesen": 1}
+            frappe.route_options = {"status": "Rückfragen", "kontaktperson": ['is', 'not set'], "ungelesen": 1, "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s6").click(function(){
-            frappe.route_options = {"status": ["not in", ["Rückfragen", "Rückfrage: Termin vereinbaren", "Eingang", "Open", "Zusammengeführt"]], "ungelesen": 1, "kontaktperson": ['is', 'not set']}
+            frappe.route_options = {"status": ["not in", ["Rückfragen", "Rückfrage: Termin vereinbaren", "Eingang", "Open", "Zusammengeführt"]], "ungelesen": 1, "kontaktperson": ['is', 'not set'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s7").click(function(){
-            frappe.route_options = {"status": 'Open', "kontaktperson": ['is', 'not set']}
+            frappe.route_options = {"status": 'Open', "kontaktperson": ['is', 'not set'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#s8").click(function(){
-            frappe.route_options = {"status": ['!=', 'Closed'], "s8": 1}
+            frappe.route_options = {"status": ['!=', 'Closed'], "s8": 1, "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         
         $("#r").click(function(){
-            frappe.route_options = {"status": 'Open'}
+            frappe.route_options = {"status": 'Open', "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r1").click(function(){
-            frappe.route_options = {"status": 'Open', 'beratung_prio': 'Hoch'}
+            frappe.route_options = {"status": 'Open', 'beratung_prio': 'Hoch', "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r2").click(function(){
-            frappe.route_options = {'status': 'Open', 'kontaktperson': 'Rechtsberatung Pool (MVBE)', 'beratung_prio': ['!=', 'Hoch']}
+            frappe.route_options = {'status': 'Open', 'kontaktperson': 'Rechtsberatung Pool (MVBE)', 'beratung_prio': ['!=', 'Hoch'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r3").click(function(){
-            frappe.route_options = {'status': 'Open', 'kontaktperson': ['!=', 'Rechtsberatung Pool (MVBE)']}
+            frappe.route_options = {'status': 'Open', 'kontaktperson': ['!=', 'Rechtsberatung Pool (MVBE)'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r4").click(function(){
-            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['is', 'set']}
+            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['is', 'set'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r5").click(function(){
-            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['is', 'set'], 'ungelesen': 1}
+            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['is', 'set'], 'ungelesen': 1, "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r6").click(function(){
-            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['!=', 'Rechtsberatung Pool (MVBE)'], 'kontaktperson': ['is', 'set'], 'ungelesen': 1}
+            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['!=', 'Rechtsberatung Pool (MVBE)'], 'kontaktperson': ['is', 'set'], 'ungelesen': 1, "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r7").click(function(){
-            frappe.route_options = {'status': ['!=', 'Closed'], 'hat_termine': 1}
+            frappe.route_options = {'status': ['!=', 'Closed'], 'hat_termine': 1, "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r8").click(function(){
-            frappe.route_options = {'status': 'Closed', 'hat_termine': 1}
+            frappe.route_options = {'status': 'Closed', 'hat_termine': 1, "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         $("#r9").click(function(){
-            frappe.route_options = {"status": ["not in", ["Rückfragen", "Open", "Zusammengeführt"]], "ungelesen": 1, "kontaktperson": ['is', 'set']}
+            frappe.route_options = {"status": ["not in", ["Rückfragen", "Open", "Zusammengeführt"]], "ungelesen": 1, "kontaktperson": ['is', 'set'], "sektion_id": "MVBE"}
             frappe.set_route("List", "Beratung", "List");
         });
         
@@ -124,7 +124,7 @@ frappe.vbz_beratung = {
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Open', 'kontaktperson': ['in', r.message]}
+                    frappe.route_options = {'status': 'Open', 'kontaktperson': ['in', r.message], "sektion_id": "MVBE"}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
@@ -136,7 +136,7 @@ frappe.vbz_beratung = {
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 0}
+                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 0, "sektion_id": "MVBE"}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
@@ -148,7 +148,7 @@ frappe.vbz_beratung = {
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 1}
+                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 1, "sektion_id": "MVBE"}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
@@ -160,7 +160,7 @@ frappe.vbz_beratung = {
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Termin vereinbart', 'kontaktperson': ['in', r.message], 'hat_termine': 1}
+                    frappe.route_options = {'status': 'Termin vereinbart', 'kontaktperson': ['in', r.message], 'hat_termine': 1, "sektion_id": "MVBE"}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
