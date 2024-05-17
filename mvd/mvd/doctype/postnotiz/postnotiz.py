b49dcbf9174a9b02495a0b6a8e3a1f341b0a9ec5
joelios <joel.mesmer@libracore.com>
2024-05-17 15:58:45 +0200

    #934 first try

---
+++ mvd/mvd/doctype/postnotiz/postnotiz.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024, libracore and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+import json
+
+class Postnotiz(Document):
+    pass
+
+def create_postnotiz(postnotiz, postretouren_log):
+    try:
+        new_postnotiz = frappe.get_doc({
+            'doctype': 'Postnotiz',
+            'postretouren_log': postretouren_log,
+            'postnotiz': json.dumps(postnotiz.__dict__)
+        })
+        
+        new_postnotiz.insert(ignore_permissions=True)
+        frappe.db.commit()
+        
+        return 1
+        
+    except Exception as err:
+        return err
---
+++ mvd/mvd/doctype/postretouren_einstellungen/postretouren_einstellungen.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, libracore and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Postretouren Einstellungen', {
+	// refresh: function(frm) {
+
+	// }
+});
