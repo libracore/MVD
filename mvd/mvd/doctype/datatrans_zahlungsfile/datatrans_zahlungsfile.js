e402f0f09841dad21b82312805ded741fa3b7e4c
joelios <joel.mesmer@libracore.com>
2024-06-28 14:53:12 +0200

    #1050 v12.13.5

--- mvd/mvd/doctype/datatrans_zahlungsfile/datatrans_zahlungsfile.js
+++ mvd/mvd/doctype/datatrans_zahlungsfile/datatrans_zahlungsfile.js
@@ -43,69 +43,75 @@ frappe.ui.form.on('Datatrans Zahlungsfile', {
         });
     },
     btn_single_report: function(frm) {
-        frappe.call({
-            method: 'create_single_report',
-            doc: frm.doc,
-            freeze: true,
-            freeze_message: "Bitte warten, der Report wird erzeugt...",
-            callback: function(response) {
-                frappe.show_alert( __("Done!") );
-                cur_frm.reload_doc();
-            }
+        frm.set_value("ignore_missing_files", 1);
+        frm.save().then(() => {
+            frappe.call({
+                method: 'create_single_report',
+                doc: frm.doc,
+                freeze: true,
+                freeze_message: "Bitte warten, der Report wird erzeugt...",
+                callback: function(response) {
+                    frappe.show_alert( __("Done!") );
+                    cur_frm.reload_doc();
+                }
+            });
         });
     },
     btn_erzeuge_reports: function(frm) {
-        frappe.prompt([
-            {'fieldname': 'year', 'fieldtype': 'Int', 'label': 'Jahr', 'reqd': 1},
-            {'fieldname': 'month', 'fieldtype': 'Select', 'label': 'Monat', 'reqd': 1, 'options': 'Januar\nFebruar\nMärz\nApril\nMai\nJuni\nJuli\nAugust\nSeptember\nOktober\nNovember\nDezember'},
-            {'fieldname': 'kommissions_prozent', 'fieldtype': 'Percent', 'label': 'Kommissions Prozent', 'reqd': 1}
-        ],
-        function(values){
-            frm.set_value("report_year", values.year);
-            frm.set_value("report_month", values.month);
-            frm.set_value("kommissions_prozent", values.kommissions_prozent);
-            frm.save().then(() => {
-                frappe.call({
-                    method: 'create_reports',
-                    doc: frm.doc,
-                    freeze: true,
-                    freeze_message: "Bitte warten, die Reporte werden erzeugt...",
-                    callback: function(response) {
-                        if (response.message == 'missing_files') {
-                            cur_frm.reload_doc();
-                            frappe.confirm(
-                                `Nachfolgende Zahlungsfiles wurden noch nicht eingelesen,<br>möchten Sie <b>trotzdem fortfahren?</b><br><br>${cur_frm.doc.missing_files}`,
-                                function(){
-                                    // on yes
-                                    frm.set_value("ignore_missing_files", 1);
-                                    frm.save().then(() => {
-                                        frappe.call({
-                                            method: 'create_reports',
-                                            doc: frm.doc,
-                                            freeze: true,
-                                            freeze_message: "Bitte warten, die Reporte werden erzeugt...",
-                                            callback: function(response) {
-                                                frappe.show_alert({message:'Done!', indicator:'green'});
-                                                cur_frm.reload_doc();
-                                            }
+        frm.set_value("ignore_missing_files", 0);
+        frm.save().then(() => {
+            frappe.prompt([
+                {'fieldname': 'year', 'fieldtype': 'Int', 'label': 'Jahr', 'reqd': 1},
+                {'fieldname': 'month', 'fieldtype': 'Select', 'label': 'Monat', 'reqd': 1, 'options': 'Januar\nFebruar\nMärz\nApril\nMai\nJuni\nJuli\nAugust\nSeptember\nOktober\nNovember\nDezember'},
+                {'fieldname': 'kommissions_prozent', 'fieldtype': 'Percent', 'label': 'Kommissions Prozent', 'reqd': 1}
+            ],
+            function(values){
+                frm.set_value("report_year", values.year);
+                frm.set_value("report_month", values.month);
+                frm.set_value("kommissions_prozent", values.kommissions_prozent);
+                frm.save().then(() => {
+                    frappe.call({
+                        method: 'create_reports',
+                        doc: frm.doc,
+                        freeze: true,
+                        freeze_message: "Bitte warten, die Reporte werden erzeugt...",
+                        callback: function(response) {
+                            if (response.message == 'missing_files') {
+                                cur_frm.reload_doc();
+                                frappe.confirm(
+                                    `Nachfolgende Zahlungsfiles wurden noch nicht eingelesen,<br>möchten Sie <b>trotzdem fortfahren?</b><br><br>${cur_frm.doc.missing_files}`,
+                                    function(){
+                                        // on yes
+                                        frm.set_value("ignore_missing_files", 1);
+                                        frm.save().then(() => {
+                                            frappe.call({
+                                                method: 'create_reports',
+                                                doc: frm.doc,
+                                                freeze: true,
+                                                freeze_message: "Bitte warten, die Reporte werden erzeugt...",
+                                                callback: function(response) {
+                                                    frappe.show_alert({message:'Done!', indicator:'green'});
+                                                    cur_frm.reload_doc();
+                                                }
+                                            });
                                         });
-                                    });
-                                },
-                                function(){
-                                    // on no
-                                    frappe.show_alert({message:'Die Report Erzeugung wurde abgebrochen', indicator:'red'});
-                                }
-                            )
-                        } else {
-                            frappe.show_alert({message:'Done!', indicator:'green'});
-                            cur_frm.reload_doc();
+                                    },
+                                    function(){
+                                        // on no
+                                        frappe.show_alert({message:'Die Report Erzeugung wurde abgebrochen', indicator:'red'});
+                                    }
+                                )
+                            } else {
+                                frappe.show_alert({message:'Done!', indicator:'green'});
+                                cur_frm.reload_doc();
+                            }
                         }
-                    }
+                    });
                 });
-            });
-        },
-        'Auswahl Report Datenbasis',
-        'Report generieren'
-        )
+            },
+            'Auswahl Report Datenbasis',
+            'Report generieren'
+            )
+        });
     }
 });
