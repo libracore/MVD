ad714c2b134062beb37a4d82406e39937678eaf4
Joel <joel.mesmer@libracore.com>
2024-09-11 10:19:48 +0200

    v13.17.0 #1127

--- mvd/mvd/doctype/postretouren_log/postretouren_log.js
+++ mvd/mvd/doctype/postretouren_log/postretouren_log.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Postretouren Log', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        if (frappe.user.has_role("System Manager")) {
+            frm.add_custom_button(__("Manuelle Verabreitung starten"), function() {
+                manual_start(frm);
+            }).addClass("btn-warning")
+        }
+    }
 });
+
+function manual_start(frm) {
+    frm.call("manual_start", {}, (r) => {
+        cur_frm.reload_doc();
+    });
+}
