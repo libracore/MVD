734509cc38d9590dcdfdb8c8392de60ec00e264c
Joel <joel.mesmer@libracore.com>
2025-03-13 11:09:23 +0100

    #1275 v16.15.9

--- mvd/__init__.py
+++ mvd/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '16.15.8'
+__version__ = '16.15.9'
--- mvd/mvd/utils/notifications.py
+++ mvd/mvd/utils/notifications.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2021, libracore and contributors
+# Copyright (c) 2025, libracore and contributors
 # For license information, please see license.txt
 import frappe
 
 def get_notification_config():
     return {
         "for_doctype": {
-            "Arbeits Backlog": {"status": 'Open'},
-            "Sales Invoice": {"posting_date": "1900-01-01"}
+            "Arbeits Backlog": "mvd.mvd.utils.notifications.get_zero_to_hide",
+            "Sales Invoice": "mvd.mvd.utils.notifications.get_zero_to_hide",
+        },
+        "for_other": {
+            "Email": "mvd.mvd.utils.notifications.get_zero_to_hide"
         }
     }
+
+def get_zero_to_hide():
+    return 0
