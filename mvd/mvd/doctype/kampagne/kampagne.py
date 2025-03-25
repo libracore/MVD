3c817a7f4026e719bc741d275c5b839d61bea2f1
Joel <joel.mesmer@libracore.com>
2025-03-25 13:28:32 +0100

    #1281

--- mvd/mvd/doctype/kampagne/kampagne.py
+++ mvd/mvd/doctype/kampagne/kampagne.py
@@ -14,13 +14,13 @@ class Kampagne(Document):
             "Email": self.email or None,
             "NewsletterName": self.newsletter_name or None,
             "CampaignTriggerCode": self.campaign_trigger_code or None,
-            "SubscribedOverPledge": 0 if cint(self.subscribed_over_pledge) != 1 else 1,
+            "SubscribedOverPledge": False if cint(self.subscribed_over_pledge) != 1 else True,
             "Zip_code": self.zip_code or 0,
             "Last_name": self.last_name or None,
             "First_name": self.first_name or None,
             "Anrede": self.anrede or None,
             "LangCode": self.lang_code or "de",
-            "Nl_abo": 0 if cint(self.nl_abo) != 1 else 1,
+            "Nl_abo": False if cint(self.nl_abo) != 1 else True,
             "Quelle": self.quelle or None
         }
         send_kampagne_to_sp(sp_data)
