0b7d949296de039cf32da14bda67dbef970df64e
RubenBeynon <ruben.beynon@mieterverband.ch>
2025-05-16 08:28:24 +0200

    rename sektion as sektion_id for consistency

--- mvd/mvd/doctype/payrexxwebhooks/payrexxwebhooks.py
+++ mvd/mvd/doctype/payrexxwebhooks/payrexxwebhooks.py
@@ -25,9 +25,9 @@ class PayrexxWebhooks(Document):
             if isinstance(mitglied_info, tuple):
                 mitglied_id, sektion_id = mitglied_info
                 frappe.db.set_value(self.doctype, self.name, "mitglied", mitglied_id)
-                frappe.db.set_value(self.doctype, self.name, "sektion", sektion_id)
+                frappe.db.set_value(self.doctype, self.name, "sektion_id", sektion_id)
             elif isinstance(mitglied_info, str):
-                frappe.db.set_value(self.doctype, self.name, "sektion", mitglied_info)
+                frappe.db.set_value(self.doctype, self.name, "sektion_id", mitglied_info)
                 
         return
     
