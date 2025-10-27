58bee530b1c16766a93da3f05dd481d121f4a93c
Joel <joel.mesmer@libracore.com>
2025-10-27 16:30:43 +0100

    divesere mrj

--- mvd/mvd/doctype/mitglied_rg_jahreslauf/mitglied_rg_jahreslauf.js
+++ mvd/mvd/doctype/mitglied_rg_jahreslauf/mitglied_rg_jahreslauf.js
@@ -2,26 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Mitglied RG Jahreslauf', {
-    refresh: function(frm) {
-        frm.call('get_overview', {}).then(r => {
-            const sektionen = r.message;
-            const mapLegend = {
-                0: "⚠️",
-                1: "✅",
-                2: ""
-            };
-            const rows = sektionen.map(s => {
-                const symbols = s.docs.map(d => mapLegend[d]).join(" ");
-                return `
-                    <tr>
-                        <td>${s.sektion}</td>
-                        <td><center>${symbols}</center></td>
-                    </tr>`
-            }).join(" ");
-            const table = `<table style="width: 50%;"><tr><th>Sektion</th><th><center>Status</center></th></tr>${rows}</table>`
-            cur_frm.set_df_property('sektions_overview', 'options', table);
-        });
-    },
     start_rg: function(frm) {
         frm.call('check_start_rg', {}).then(r => {
             if (r.message > 0) {
