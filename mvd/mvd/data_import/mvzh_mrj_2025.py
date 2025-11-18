f0dd483acf037cf1a9d253c95b2529363d50ba91
Joel <joel.mesmer@libracore.com>
2025-11-18 12:53:25 +0100

    #1496

--- mvd/mvd/data_import/mvzh_mrj_2025.py
+++ mvd/mvd/data_import/mvzh_mrj_2025.py
@@ -44,12 +44,12 @@ def read_csv(file_name, site_name='libracore.mieterverband.ch', limit=False, ben
         for index, row in filtered.iterrows():
             print(mvzh_rg.mitglied_nr, row["faktnr_MG"])
 
-            # frappe.db.sql("""UPDATE `tabSales Invoice` SET `docstatus` = 0 WHERE `name` = '{sinv}'""".format(sinv=mvzh_rg.sinv), as_list=True)
-            # frappe.db.commit()
+            frappe.db.sql("""UPDATE `tabSales Invoice` SET `docstatus` = 0 WHERE `name` = '{sinv}'""".format(sinv=mvzh_rg.sinv), as_list=True)
+            frappe.db.commit()
             sinv = frappe.get_doc("Sales Invoice", mvzh_rg.sinv)
-            sinv.docstatus = 0
-            sinv.save()
-            sinv.reload()
+            # sinv.docstatus = 0
+            # sinv.save()
+            # sinv.reload()
 
             sinv.esr_reference = row["vESRReferenzNr_MG"]
             sinv.mvzh_sinv_nr = row["faktnr_MG"]
@@ -59,6 +59,8 @@ def read_csv(file_name, site_name='libracore.mieterverband.ch', limit=False, ben
                 for item in sinv.items:
                     if item.rate > 0:
                         item.rate = int(str(row["betrag_mg"]).replace(" 00", ""))
+            
+            sinv.outstanding_amount = 0
             
             sinv.save()
             sinv.reload()
