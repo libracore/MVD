b617746d16b6bed63ad56b4fd5c7d909084e5d4d
Joel <joel.mesmer@libracore.com>
2025-12-11 11:19:13 +0100

    #1567

---
+++ mvd/mvd/doctype/db_backuprestore/db_backuprestore.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2025, libracore and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('DB BackupRestore', {
+    create_json: function(frm) {
+        frappe.dom.freeze('Bitte warten, das JSON-File wird erzeugt...');
+        frm.call('create_json', {}).then(r => {
+            frappe.dom.unfreeze();
+            cur_frm.reload_doc();
+            frappe.msgprint("Das JSON wurde erzeugt");
+        });
+    },
+    load_json: function(frm) {
+        frappe.confirm(
+            '<b>Achtung:</b> Alle Daten in der DB werden durch jene im JSON ersetzt!<br>Wollen Sie fortfahren?',
+            function(){
+                // on yes
+                frappe.dom.freeze('Bitte warten, das JSON-File wird eingelesen...');
+                frm.call('load_json', {}).then(r => {
+                    frappe.dom.unfreeze();
+                    frappe.msgprint("Das JSON wurde eingelesen");
+                });
+            },
+            function(){
+                // on no
+                frappe.show_alert('Einlesen abgebrochen')
+            }
+        )
+    }
+});
