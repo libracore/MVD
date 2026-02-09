a36fb5dad4c883c5151027a7fc886d3bbe0d8043
Joel <joel.mesmer@libracore.com>
2026-02-09 20:12:36 +0100

    create_mandat Workflow

--- mvd/mvd/doctype/beratung/beratung.js
+++ mvd/mvd/doctype/beratung/beratung.js
@@ -439,6 +439,9 @@ frappe.ui.form.on('Beratung', {
         // Add BTN Admin ToDo
         frm.add_custom_button(__("Erstelle ToDo"),  function() {
             erstelle_todo(frm);
+        // Add BTN Mandat
+        frm.add_custom_button(__("Mandat"),  function() {
+            create_mandat(frm);
         });
     },
     mv_mitgliedschaft: function(frm) {
