2e0c1f87c492007f59340aa32e8eac9906000fc3
Christoph Laszlo <christoph.laszlo@mieterverband.ch>
2026-02-09 21:31:13 +0100

    button mandat besser verschoben

--- mvd/mvd/doctype/beratung/beratung.js
+++ mvd/mvd/doctype/beratung/beratung.js
@@ -417,12 +417,6 @@ frappe.ui.form.on('Beratung', {
                 //~ }
             //~ )
         //~ }
-        
-        // Add BTN Admin ToDo
-        frm.add_custom_button(__("Erstelle ToDo"),  function() {
-            erstelle_todo(frm);
-        });
-        
         // Add BTN Admin ToDo
         frm.add_custom_button(__("Erstelle ToDo"),  function() {
             erstelle_todo(frm);
