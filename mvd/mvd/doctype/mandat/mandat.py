f9a4b7618fa427f7336ee4a5d56c24cd804067fd
RubenBeynon <ruben.beynon@mieterverband.ch>
2026-02-26 15:27:45 +0100

    nun werden auch emails gesendet, wenn zuerst keine Kontaktperson gesetzt war und diese im Nachhinein gesetzt wird

--- mvd/mvd/doctype/mandat/mandat.py
+++ mvd/mvd/doctype/mandat/mandat.py
@@ -58,8 +58,6 @@ def create_mandat(sektion, beratung, mitglied, berater_in, typ, bemerkung, perso
     mandat.persoenliche_bemerkung = persoenliche_bemerkung
 
     mandat.insert(ignore_permissions=True)
-    if mandat.typ == "Rechtsschutzversicherung":
-        send_confirmation_email(mandat)
 
     return mandat.name
 
