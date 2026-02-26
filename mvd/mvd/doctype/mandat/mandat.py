dc5df001fd34a0dd2e89b9326d8db1a1ca225063
RubenBeynon <ruben.beynon@mieterverband.ch>
2026-02-26 12:13:02 +0100

    Email an KP und Mitglied stehen

--- mvd/mvd/doctype/mandat/mandat.py
+++ mvd/mvd/doctype/mandat/mandat.py
@@ -58,6 +58,8 @@ def create_mandat(sektion, beratung, mitglied, berater_in, typ, bemerkung, perso
     mandat.persoenliche_bemerkung = persoenliche_bemerkung
 
     mandat.insert(ignore_permissions=True)
+    if mandat.typ == "Rechtsschutzversicherung":
+        send_confirmation_email(mandat)
 
     return mandat.name
 
