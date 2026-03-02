20b2b7f0246b8458d7a5cb8a0ccb52ec6dce0b4e
Joel <joel.mesmer@libracore.com>
2026-03-02 15:31:52 +0100

    rb #1607

--- mvd/hooks.py
+++ mvd/hooks.py
@@ -72,7 +72,8 @@ jenv = {
         "get_gesamtbetrag_gemahnte_rechnungen:mvd.mvd.doctype.druckvorlage.druckvorlage.get_gesamtbetrag_gemahnte_rechnungen",
         "get_rechnungsdatum:mvd.mvd.doctype.druckvorlage.druckvorlage.get_rechnungsdatum",
         "get_jahresrechnung_jahr:mvd.mvd.doctype.druckvorlage.druckvorlage.get_jahresrechnung_jahr",
-        "get_beratungs_daten:mvd.mvd.doctype.druckvorlage.druckvorlage.get_beratungs_daten"
+        "get_beratungs_daten:mvd.mvd.doctype.druckvorlage.druckvorlage.get_beratungs_daten",
+        "get_mahngebuehr:mvd.mvd.doctype.druckvorlage.druckvorlage.get_mahngebuehr"
     ]
 }
 
