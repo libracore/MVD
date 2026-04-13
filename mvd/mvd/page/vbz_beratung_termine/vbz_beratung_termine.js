ea22a1418bf067628793e517cfa6b8cc4537226e
Joel <joel.mesmer@libracore.com>
2026-04-13 19:50:08 +0200

    Rebase #1734

--- mvd/mvd/page/vbz_beratung_termine/vbz_beratung_termine.js
+++ mvd/mvd/page/vbz_beratung_termine/vbz_beratung_termine.js
@@ -1,56 +1,82 @@
 frappe.pages['vbz_beratung_termine'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Verarbeitungszentrale',
-		single_column: true
-	});
-    frappe.vbz_beratung_termine.add_views(page);
-    localStorage['firstLoad'] = true;
-}
-frappe.pages['vbz_beratung_termine'].refresh= function(wrapper){
-    frappe.vbz_beratung_termine.show_view('vbz_beratung_termine');
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Verarbeitungszentrale',
+        single_column: true
+    });
+
+    frappe.vbz_beratung_termine.page = page;
+    frappe.vbz_beratung_termine.render_view(page);
+    poll(page);
+};
+
+frappe.pages['vbz_beratung_termine'].refresh = function(wrapper) {
     frappe.dom.unfreeze();
-} 
+};
 
 frappe.vbz_beratung_termine = {
-    add_views: function(page) {
+    page: null,
+
+    render_view: function(page) {
         frappe.call({
-            'method': "mvd.mvd.page.vbz_beratung_termine.vbz_beratung_termine.get_open_data",
-            'args': {},
-            'freeze': true,
-            'freeze_message': 'Lade Verarbeitungszentrale...',
-            'async': false,
-            'callback': function(r)
-            {
+            method: "mvd.mvd.page.vbz_beratung_termine.vbz_beratung_termine.get_open_data",
+            args: {},
+            freeze: true,
+            freeze_message: 'Lade Verarbeitungszentrale...',
+            async: false,
+            callback: function(r) {
                 if (r.message) {
-                    page.add_view('vbz_beratung_termine', frappe.render_template("vbz_beratung_termine", eval(r.message)))
+                    const html = frappe.render_template("vbz_beratung_termine", r.message);
+
+                    // Alten Inhalt entfernen und neu rendern
+                    $(page.main).empty().html(html);
+
                     frappe.vbz_beratung_termine.add_click_handlers();
-                    localStorage['firstLoad'] = true;
+                    localStorage['datenstand_for_polling'] = r.message.datenstand_for_polling;
                 }
+
+                frappe.dom.unfreeze();
             }
         });
     },
-    show_view: function(view) {
-        cur_page.page.page.set_view(view);
-    },
-    remove_click_handlers: function() {
 
+    reload_view: function(page) {
+        this.render_view(page);
     },
+
     add_click_handlers: function() {
         $(".termin-tr").each(function() {
             if ($(this).attr('data-beratung')) {
-                $(this).click(function(){
+                $(this).off('click').on('click', function() {
                     if ($(this).attr('data-beratung') != '---') {
                         frappe.set_route("Form", "Beratung", $(this).attr('data-beratung'));
                     } else {
                         frappe.msgprint("Kein Absprung zur Beratung möglich, da dieser Termin noch frei ist.");
                     }
-                })
+                });
             }
         });
-        frappe.dom.unfreeze();
     },
+
     open_beratung: function(beratung) {
         frappe.set_route("Form", "Beratung", beratung.beratung);
     }
+};
+
+async function poll(page) {
+    try {
+        if (frappe.get_route_str() !== 'vbz_beratung_termine') return;
+
+        const since = localStorage['datenstand_for_polling'];
+        const res = await fetch(`/api/method/mvd.mvd.page.vbz_beratung_termine.vbz_beratung_termine.has_changed?since=${since}`);
+        const data = await res.json();
+
+        if (data.message > 0) {
+            frappe.vbz_beratung_termine.reload_view(page);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
+    setTimeout(() => poll(page), 2000);
 }
