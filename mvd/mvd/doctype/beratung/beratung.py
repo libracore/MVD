b2566e7dee164c2a14e07c80743d70db65193681
Joel <joel.mesmer@libracore.com>
2026-04-20 13:40:52 +0200

    #1746

--- mvd/mvd/doctype/beratung/beratung.py
+++ mvd/mvd/doctype/beratung/beratung.py
@@ -766,7 +766,7 @@ def create_neue_beratung(termin_block_data, art, ort, berater_in, telefonnummer,
                 row.telefonnummer = telefonnummer
                 row.abp_referenz = termin['referenz']
                 row.notiz = notiz
-                row.wunsch_berater_in = wunsch_berater_in if wunsch_berater_in else ''
+                row.wunsch_berater_in = wunsch_berater_in.replace("({0})".format(sektion_id), "") if wunsch_berater_in else ''
             
             beratung.save()
         else:
@@ -782,7 +782,7 @@ def create_neue_beratung(termin_block_data, art, ort, berater_in, telefonnummer,
                 row.telefonnummer = telefonnummer
                 row.abp_referenz = termin['referenz']
                 row.notiz = notiz
-                row.wunsch_berater_in = wunsch_berater_in if wunsch_berater_in else ''
+                row.wunsch_berater_in = wunsch_berater_in.replace("({0})".format(sektion_id), "") if wunsch_berater_in else ''
             beratung.beratungskanal = "Telefon" if art == 'telefonisch' else art
             beratung.save()
         
