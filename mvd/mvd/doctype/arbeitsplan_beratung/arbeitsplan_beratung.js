7918121e60d407f0a539436a6dfba3d4ad255a8d
Joel <joel.mesmer@libracore.com>
2026-05-12 12:56:24 +0200

    #1761

--- mvd/mvd/doctype/arbeitsplan_beratung/arbeitsplan_beratung.js
+++ mvd/mvd/doctype/arbeitsplan_beratung/arbeitsplan_beratung.js
@@ -36,16 +36,6 @@ frappe.ui.form.on('Arbeitsplan Beratung', {
                 )
             }
         });
-
-        frm.add_custom_button(__("Termine aus Excel importieren"), function() {
-            if (cur_frm.is_dirty()) {
-                frappe.throw("Bitte speichern Sie den Arbeitsplan zuerst.")
-            } else {
-                frm.call("import_from_excel", {}, (r) => {
-                    frm.reload_doc();
-                });
-            }
-        });
         
         if (cur_frm.doc.einteilung.length > 0) {
             // holen der bereits verwendeten Termin-Blocks (um zu verhindern dass diese gelöscht/geändert werden)
