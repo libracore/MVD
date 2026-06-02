a8b9f4920c44de8cce024ac3ebf4244e665c8eaf
Joel <joel.mesmer@libracore.com>
2026-06-02 20:43:10 +0200

    Dokumentenvorlage #1774

--- mvd/mvd/doctype/beratung/beratung.js
+++ mvd/mvd/doctype/beratung/beratung.js
@@ -434,6 +434,23 @@ frappe.ui.form.on('Beratung', {
         frm.add_custom_button(__("Erstelle ToDo"),  function() {
             erstelle_todo(frm);
         });
+
+        frm.add_custom_button(__("Erstelle Schadenanzeige"), function() {
+            frappe.call({
+                method: "mvd.mvd.utils.document_template_handler.use_template",
+                args:{
+                        template: "Schadenanzeige",
+                        source_doc: cur_frm.doc.name,
+                        source_dt: "Beratung"
+                },
+                freeze: true,
+                freeze_message: 'Verarbeite Vorlage...',
+                callback: function(r)
+                {
+                    cur_frm.reload_doc();
+                }
+            });
+        });
     },
     mv_mitgliedschaft: function(frm) {
         if ((!frm.doc.__islocal)&&(cur_frm.doc.mv_mitgliedschaft)) {
