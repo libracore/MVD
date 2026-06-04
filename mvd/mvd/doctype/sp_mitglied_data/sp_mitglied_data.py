67182d207f14e6261672724adcb60c66b657bb15
Joel <joel.mesmer@libracore.com>
2026-06-04 09:00:17 +0200

    Error Catch in fixing_wrong_data

--- mvd/mvd/doctype/sp_mitglied_data/sp_mitglied_data.py
+++ mvd/mvd/doctype/sp_mitglied_data/sp_mitglied_data.py
@@ -114,76 +114,80 @@ def fixing_wrong_data(manual=False):
     if manual:
         print("Analyse DB, please wait...")
     
-    mitgliedschaften = frappe.db.sql(
-        """
-            SELECT
-                spx.ref_nr,
-                spx.mitgliedId,
-                spx.status
-            FROM (
+    try:
+        mitgliedschaften = frappe.db.sql(
+            """
                 SELECT
-                    sp.`name` AS ref_nr,
-                    JSON_UNQUOTE(JSON_EXTRACT(sp.`json`, '$.mitgliedId')) AS mitgliedId,
-                    CASE JSON_UNQUOTE(JSON_EXTRACT(sp.`json`, '$.status'))
-                        WHEN 'Regulaer' THEN 'Regulär'
-                        WHEN 'OnlineAnmeldung' THEN 'Online-Anmeldung'
-                        WHEN 'OnlineBeitritt' THEN 'Online-Beitritt'
-                        WHEN 'OnlineKuendigung' THEN 'Online-Kündigung'
-                        WHEN 'Kuendigung' THEN 'Kündigung'
-                        WHEN 'InteressentIn' THEN 'Interessent*in'
-                        ELSE JSON_UNQUOTE(JSON_EXTRACT(sp.`json`, '$.status'))
-                    END AS status
-                FROM `tabSP Mitglied Data` sp
-            ) spx
-            LEFT JOIN `tabMitgliedschaft` m
-                ON m.`mitglied_nr` = spx.ref_nr
-                AND m.`status_c` = spx.status
-                AND m.`mitglied_id` = spx.mitgliedId
-            WHERE m.`mitglied_nr` IS NULL;
-        """,
-        as_dict=True
-    )
+                    spx.ref_nr,
+                    spx.mitgliedId,
+                    spx.status
+                FROM (
+                    SELECT
+                        sp.`name` AS ref_nr,
+                        JSON_UNQUOTE(JSON_EXTRACT(sp.`json`, '$.mitgliedId')) AS mitgliedId,
+                        CASE JSON_UNQUOTE(JSON_EXTRACT(sp.`json`, '$.status'))
+                            WHEN 'Regulaer' THEN 'Regulär'
+                            WHEN 'OnlineAnmeldung' THEN 'Online-Anmeldung'
+                            WHEN 'OnlineBeitritt' THEN 'Online-Beitritt'
+                            WHEN 'OnlineKuendigung' THEN 'Online-Kündigung'
+                            WHEN 'Kuendigung' THEN 'Kündigung'
+                            WHEN 'InteressentIn' THEN 'Interessent*in'
+                            ELSE JSON_UNQUOTE(JSON_EXTRACT(sp.`json`, '$.status'))
+                        END AS status
+                    FROM `tabSP Mitglied Data` sp
+                ) spx
+                LEFT JOIN `tabMitgliedschaft` m
+                    ON m.`mitglied_nr` = spx.ref_nr
+                    AND m.`status_c` = spx.status
+                    AND m.`mitglied_id` = spx.mitgliedId
+                WHERE m.`mitglied_nr` IS NULL;
+            """,
+            as_dict=True
+        )
 
-    affected_qty = 0
-    affected_records = []
+        affected_qty = 0
+        affected_records = []
 
-    if not manual:
-        for mitgliedschaft in mitgliedschaften:
-            mitglied_id = get_mitglied_id_from_nr(mitglied_nr=mitgliedschaft.ref_nr, ignore_inaktiv=True)
-            if cint(frappe.db.get_value("Mitgliedschaft", mitglied_id, "validierung_notwendig")) != 1:
-                create_or_update_sp_mitglied_data(mitgliedschaft.ref_nr, mitglied_id)
-                # Es gibt einen BG-Prozess welcher ~ alle 4' ausgeführt wird und die "SP Mitglied Data" aktualisiert insofern notwendig.
-                # Es kann vorkommen, dass dieser nächtliche Korrekturlauf "SP Mitglied Data" korrigiert, welche bereits zur Aktualisierung markiert waren.
-                # Wenn dies der Fall ist, wurden diese als falsch-positive Fehler geloggt.
-                # Durch das unten folgende IF werden solche falsch-positiven Fehler-Logs vermieden.
-                if (
-                    not frappe.db.exists("SP Mitglied Data", mitgliedschaft.ref_nr)
-                    or cint(frappe.db.get_value("SP Mitglied Data", mitgliedschaft.ref_nr, "needs_update")) != 1
-                ):
-                    affected_qty += 1
-                    affected_records.append(mitgliedschaft.ref_nr)
-    else:
-        for mitgliedschaft in tqdm(mitgliedschaften, desc="fixing_wrong_data", unit=" Corr.", total=len(mitgliedschaften)):
-            mitglied_id = get_mitglied_id_from_nr(mitglied_nr=mitgliedschaft.ref_nr, ignore_inaktiv=True)
-            if cint(frappe.db.get_value("Mitgliedschaft", mitglied_id, "validierung_notwendig")) != 1:
-                create_or_update_sp_mitglied_data(mitgliedschaft.ref_nr, mitglied_id)
-                # Es gibt einen BG-Prozess welcher ~ alle 4' ausgeführt wird und die "SP Mitglied Data" aktualisiert insofern notwendig.
-                # Es kann vorkommen, dass dieser nächtliche Korrekturlauf "SP Mitglied Data" korrigiert, welche bereits zur Aktualisierung markiert waren.
-                # Wenn dies der Fall ist, wurden diese als falsch-positive Fehler geloggt.
-                # Durch das unten folgende IF werden solche falsch-positiven Fehler-Logs vermieden.
-                if (
-                    not frappe.db.exists("SP Mitglied Data", mitgliedschaft.ref_nr)
-                    or cint(frappe.db.get_value("SP Mitglied Data", mitgliedschaft.ref_nr, "needs_update")) != 1
-                ):
-                    affected_qty += 1
-                    affected_records.append(mitgliedschaft.ref_nr)
-    
-    if manual:
-        print("{0} korrigiert".format(affected_qty))
-        print(affected_records)
-    
-    if affected_qty > 0:
-        frappe.log_error("Anzahl korrigiert: {0}\nDatensätze: {1}".format(affected_qty, affected_records), 'SP Mitlgied Data fixing_wrong_data')
+        if not manual:
+            for mitgliedschaft in mitgliedschaften:
+                mitglied_id = get_mitglied_id_from_nr(mitglied_nr=mitgliedschaft.ref_nr, ignore_inaktiv=True)
+                if cint(frappe.db.get_value("Mitgliedschaft", mitglied_id, "validierung_notwendig")) != 1:
+                    create_or_update_sp_mitglied_data(mitgliedschaft.ref_nr, mitglied_id)
+                    # Es gibt einen BG-Prozess welcher ~ alle 4' ausgeführt wird und die "SP Mitglied Data" aktualisiert insofern notwendig.
+                    # Es kann vorkommen, dass dieser nächtliche Korrekturlauf "SP Mitglied Data" korrigiert, welche bereits zur Aktualisierung markiert waren.
+                    # Wenn dies der Fall ist, wurden diese als falsch-positive Fehler geloggt.
+                    # Durch das unten folgende IF werden solche falsch-positiven Fehler-Logs vermieden.
+                    if (
+                        not frappe.db.exists("SP Mitglied Data", mitgliedschaft.ref_nr)
+                        or cint(frappe.db.get_value("SP Mitglied Data", mitgliedschaft.ref_nr, "needs_update")) != 1
+                    ):
+                        affected_qty += 1
+                        affected_records.append(mitgliedschaft.ref_nr)
+        else:
+            for mitgliedschaft in tqdm(mitgliedschaften, desc="fixing_wrong_data", unit=" Corr.", total=len(mitgliedschaften)):
+                mitglied_id = get_mitglied_id_from_nr(mitglied_nr=mitgliedschaft.ref_nr, ignore_inaktiv=True)
+                if cint(frappe.db.get_value("Mitgliedschaft", mitglied_id, "validierung_notwendig")) != 1:
+                    create_or_update_sp_mitglied_data(mitgliedschaft.ref_nr, mitglied_id)
+                    # Es gibt einen BG-Prozess welcher ~ alle 4' ausgeführt wird und die "SP Mitglied Data" aktualisiert insofern notwendig.
+                    # Es kann vorkommen, dass dieser nächtliche Korrekturlauf "SP Mitglied Data" korrigiert, welche bereits zur Aktualisierung markiert waren.
+                    # Wenn dies der Fall ist, wurden diese als falsch-positive Fehler geloggt.
+                    # Durch das unten folgende IF werden solche falsch-positiven Fehler-Logs vermieden.
+                    if (
+                        not frappe.db.exists("SP Mitglied Data", mitgliedschaft.ref_nr)
+                        or cint(frappe.db.get_value("SP Mitglied Data", mitgliedschaft.ref_nr, "needs_update")) != 1
+                    ):
+                        affected_qty += 1
+                        affected_records.append(mitgliedschaft.ref_nr)
+        
+        if manual:
+            print("{0} korrigiert".format(affected_qty))
+            print(affected_records)
+        
+        if affected_qty > 0:
+            frappe.log_error("Anzahl korrigiert: {0}\nDatensätze: {1}".format(affected_qty, affected_records), 'SP Mitlgied Data fixing_wrong_data')
+    except Exception as err:
+        frappe.log_error("{0}".format(frappe.get_traceback()), 'Failed: SP Mitlgied Data fixing_wrong_data')
+        pass
     
     # Um Race Conditions zu vermeiden, wird im DT Race Condition Helper das aktuelle Datum hinterlegt, sobald der Prozess durch ist.
     # Dies ermöglicht in gewissen Fällen ein Serielles Abarbeiten von BG-Jobs obwohl mehrere Worker paralell arbeiten.
