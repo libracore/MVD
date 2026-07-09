de2298c96e5e939bca11d4d77d32cd2db2e1ddfd
RubenBeynon <ruben.beynon@mieterverband.ch>
2026-07-09 11:54:26 +0200

    kopiere vbz von Bern

--- mvd/mvd/page/vbz_beratung_mvzh/vbz_beratung_mvzh.js
+++ mvd/mvd/page/vbz_beratung_mvzh/vbz_beratung_mvzh.js
@@ -38,172 +38,129 @@ frappe.vbz_beratung_mvzh = {
     },
     add_click_handlers: function(open_datas) {
         //~ frappe.vbz_beratung_mvzh.remove_click_handlers();
+        var sektion = frappe.boot.default_sektion || "MVZH";
 
-        // Define the function to hide elements based on the section
-        function hideElementsBasedOnSection() {
-            let ausgeblendete_elemente = {'MVSH': ['s2','a1','r7','r8','p4']};
-            let sektion = frappe.boot.default_sektion;
 
-            if (sektion in ausgeblendete_elemente) {
-                ausgeblendete_elemente[sektion].forEach(element => {
-                    let elementId = `${element}_as`;
-                    document.getElementById(elementId).style.display = 'none';
-                    console.log(`Element ${elementId} hidden`);
-                });
-            }
-        }
+        $("#s1").click(function(){
+            frappe.route_options = {"status": 'Eingang', "mv_mitgliedschaft": ['is', 'not set'], "sektion_id": sektion}
+            frappe.set_route("List", "Beratung", "List");
+        });
+        $("#s6").click(function(){
+            frappe.route_options = {"status": ["not in", ["Rückfragen", "Rückfrage: Termin vereinbaren", "Eingang", "Open", "Zusammengeführt"]], "ungelesen": 1, "kontaktperson": ['is', 'not set'], "sektion_id": sektion}
+            frappe.set_route("List", "Beratung", "List");
+        });
+        $("#s10").click(function(){
+            frappe.call({
+                method: "mvd.mvd.page.vbz_beratung_mvzh.vbz_beratung_mvzh.get_termine_in_zukunft",
+                args: {},
+                callback: function(r) {
+                    var parents = r.message || [];
+                    if (parents.length === 0) {
+                        parents = [""];
+                    }
+                    frappe.route_options = {
+                        "ungelesen": 1,
+                        "sektion_id": sektion,
+                        "name": ["in", parents]
+                    }
+                    frappe.set_route("List", "Beratung", "List");
+                }
+            });
+        });
 
-        // Call the function where needed, for example, in the add_views function
-        hideElementsBasedOnSection();
-                
-        $("#s_as").click(function(){
-            frappe.route_options = {"status": 'Eingang'}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#s1_as").click(function(){
-            frappe.route_options = {"status": 'Eingang', "mv_mitgliedschaft": ['is', 'not set']}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#s2_as").click(function(){
-            frappe.route_options = {"status": ['!=', 'Closed'], "s8": 1}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#s3_as").click(function(){
-            frappe.route_options = {"status": 'Zusammengeführt', "ungelesen": 1}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#s3").click(function(){
-            frappe.route_options = {"status": 'Rückfrage: Termin vereinbaren'}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#s4_as").click(function(){
-            frappe.route_options = {"status": "Rückfragen", "kontaktperson": ['is', 'not set'], "ungelesen": 0}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#s5_as").click(function(){
-            frappe.route_options = {"status": "Rückfragen", "kontaktperson": ['is', 'not set'], "ungelesen": 1}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#a1_as").click(function(){
-            frappe.route_options = {"status": "Open", "kontaktperson": ['like', 'Administration%']}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        // $("#s6_as").click(function(){
-        //     frappe.route_options = {"status": ["not in", ["Rückfragen", "Rückfrage: Termin vereinbaren", "Eingang", "Open", "Zusammengeführt"]], "ungelesen": 1, "kontaktperson": ['is', 'not set']}
-        //     frappe.set_route("List", "Beratung", "List");
-        // });
-        // $("#s7_as").click(function(){
-        //     frappe.route_options = {"status": 'Open', "kontaktperson": ['is', 'not set']}
-        //     frappe.set_route("List", "Beratung", "List");
-        // });
         
-        $("#r_as").click(function(){
-            frappe.route_options = {"status": 'Open'}
+        $("#r").click(function(){
+            frappe.route_options = {"status": ['in', ['Open', 'In Arbeit']], "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r1_as").click(function(){
-            frappe.route_options = {"status": 'Open', 'beratung_prio': 'Hoch'}
+        $("#r1").click(function(){
+            frappe.route_options = {"status": ['in', ['Open', 'In Arbeit']], 'beratung_prio': 'Hoch', "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r2_as").click(function(){
-            frappe.route_options = {'status': 'Open', 'kontaktperson': ['like','Rechtsberatung Pool%'], 'beratung_prio': ['!=', 'Hoch']}
+        $("#r2").click(function(){
+             frappe.route_options = {'status': ['in', ['Open', 'In Arbeit']], 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'beratung_prio': ['not in', ['Hoch']], "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-           $("#r2_bs").click(function(){
-            frappe.route_options = {'status': 'Open', 'kontaktperson': ['like','Rechtsberatung Pool%'], 'beratung_prio': ['!=', 'Hoch'], '_user_tags': ['not like', '%MNE-MVBS%']}
+        $("#r3").click(function(){
+            frappe.route_options = {'status': ['in', ['Open', 'In Arbeit']], 'r3': 1, "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r3_as").click(function(){
-            frappe.route_options = {'status': 'Open', 'r3': 1}
+        $("#r4").click(function(){
+            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['is', 'set'], "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r4_as").click(function(){
-            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['is', 'set']}
+        $("#r5").click(function(){
+            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'ungelesen': 1, "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r5_as").click(function(){
-            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'ungelesen': 1}
+        $("#r6").click(function(){
+            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['not like', 'Rechtsberatung Pool%'], 'ungelesen': 1, "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r6_as").click(function(){
-            frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['not like', 'Rechtsberatung Pool%'], 'kontaktperson': ['is', 'set'], 'ungelesen': 1}
+        $("#r7").click(function(){
+            frappe.route_options = {'status': ['!=', 'Closed'], 'hat_termine': 1, "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r7_as").click(function(){
-            frappe.route_options = {'status': ['!=', 'Closed'], 'hat_termine': 1}
+        $("#r8").click(function(){
+            frappe.route_options = {'status': 'Closed', 'hat_termine': 1, "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r8_as").click(function(){
-            frappe.route_options = {'status': 'Closed', 'hat_termine': 1}
+        $("#r9").click(function(){
+            frappe.route_options = {"status": ["not in", ["Rückfragen", "Open", "Zusammengeführt", "Termin vereinbart", "Rückfrage: Termin vereinbaren"]], "ungelesen": 1, "kontaktperson": ['is', 'set'], "sektion_id": sektion}
             frappe.set_route("List", "Beratung", "List");
         });
-        $("#r9_as").click(function(){
-            frappe.route_options = {"status": ["not in", ["Rückfragen", "Open", "Zusammengeführt", "Termin vereinbart"]], "ungelesen": 1, "kontaktperson": ['is', 'set']}
-            frappe.set_route("List", "Beratung", "List");
-        });
-        $("#r10_bs").click(function(){
-            frappe.route_options = {'status': ['!=', 'Closed'], '_user_tags': ['like', '%MNE-MVBS%']};
-            frappe.set_route("List", "Beratung");
-        });
-        $("#r11_bs").click(function(){
-            frappe.route_options = {'status': ['!=', 'Closed'], '_user_tags': ['like', '%Mandat-MVBS%']};
-            frappe.set_route("List", "Beratung");
-        });
-        $("#r12_bs").click(function(){
-            frappe.route_options = {'status': ['!=', 'Closed'], '_user_tags': ['like', '%PHF-MVBS%']};
-            frappe.set_route("List", "Beratung");
-        });
-        
-        $("#p1_as").click(function(){
+     
+
+        $("#p1").click(function(){
             frappe.call({
                 'method': "mvd.mvd.page.vbz_beratung_mvzh.vbz_beratung_mvzh.get_user_kontaktperson",
                 'args': {'only_session_user': 1},
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Open', 'kontaktperson': ['in', r.message]}
+                    frappe.route_options = {'status': ['in', ['Open', 'In Arbeit']], 'kontaktperson': ['in', r.message], "sektion_id": sektion}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
         });
-        $("#p2_as").click(function(){
+        $("#p2").click(function(){
             frappe.call({
                 'method': "mvd.mvd.page.vbz_beratung_mvzh.vbz_beratung_mvzh.get_user_kontaktperson",
                 'args': {},
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 0}
+                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 0, "sektion_id": sektion}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
         });
-        $("#p3_as").click(function(){
+        $("#p3").click(function(){
             frappe.call({
                 'method': "mvd.mvd.page.vbz_beratung_mvzh.vbz_beratung_mvzh.get_user_kontaktperson",
                 'args': {},
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 1}
+                    frappe.route_options = {'status': 'Rückfragen', 'kontaktperson': ['in', r.message], 'ungelesen': 1, "sektion_id": sektion}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
         });
-        $("#p4_as").click(function(){
+        $("#p4").click(function(){
             frappe.call({
                 'method': "mvd.mvd.page.vbz_beratung_mvzh.vbz_beratung_mvzh.get_user_kontaktperson",
                 'args': {},
                 'async': false,
                 'callback': function(r)
                 {
-                    frappe.route_options = {'status': 'Termin vereinbart', 'kontaktperson': ['in', r.message], 'hat_termine': 1}
+                    frappe.route_options = {'status': 'Termin vereinbart', 'kontaktperson': ['in', r.message], 'hat_termine': 1, "sektion_id": sektion}
                     frappe.set_route("List", "Beratung", "List");
                 }
             });
         });
-        
-        
+
+             
         $("#rechtsberaterinnen_as").click(function(){
             frappe.set_route("List", "Termin Kontaktperson", "List");
         });
--- mvd/mvd/page/vbz_beratung_mvzh/vbz_beratung_mvzh.py
+++ mvd/mvd/page/vbz_beratung_mvzh/vbz_beratung_mvzh.py
@@ -12,33 +12,28 @@ from frappe.utils.pdf import get_file_data_from_writer
 def get_open_data():
     open_data = {
         'beratung': {
-            'datenstand_as': now_datetime().strftime("%d.%m.%Y %H:%M:%S"),
-            's_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Eingang'}, limit=100, distinct=True)),
-            's1_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Eingang', 'mv_mitgliedschaft': ['is', 'not set']}, limit=100, distinct=True)),
-            's2_as': len(frappe.get_list('Beratung', fields='name', filters={'status': ['!=', 'Closed'], 's8': 1}, limit=100, distinct=True)),
-            's3_as': len(frappe.get_list('Beratung', fields='name', filters={'ungelesen': 1, 'status': 'Zusammengeführt'}, limit=100, distinct=True)),
-            's3': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfrage: Termin vereinbaren'}, limit=100, distinct=True)),
-            's4_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['is', 'not set'], 'ungelesen': 0}, limit=100, distinct=True)),
-            's5_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['is', 'not set'], 'ungelesen': 1}, limit=100, distinct=True)),
-            'a1_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Open', 'kontaktperson': ['like', 'Administration%']}, limit=100, distinct=True)),
-            'r_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Open'}, limit=100, distinct=True)),
-            'r1_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Open', 'beratung_prio': 'Hoch'}, limit=100, distinct=True)),
-            'r2_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Open', 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'beratung_prio': ['!=', 'Hoch']}, limit=100, distinct=True)),
-            'r2_bs': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Open', 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'beratung_prio': ['!=', 'Hoch'], '_user_tags': ['not like', '%MNE-MVBS%']}, limit=100, distinct=True)),
-            'r3_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Open', 'r3': 1}, limit=100, distinct=True)),
-            'r4_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['is', 'set'], 'ungelesen': 0}, limit=100, distinct=True)),
-            'r5_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'ungelesen': 1}, limit=100, distinct=True)),
-            'r6_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['not like', 'Rechtsberatung Pool%'], 'ungelesen': 1}, limit=100, distinct=True)),
-            'r7_as': len(frappe.get_list('Beratung', fields='name', filters={'status': ['!=', 'Closed'], 'hat_termine': 1}, limit=100, distinct=True)),
-            'r8_as': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Closed', 'hat_termine': 1}, limit=100, distinct=True)),
-            'r9_as': len(frappe.get_list('Beratung', fields='name', filters={'status': ['not in', ['Rückfragen', 'Open', 'Zusammengeführt', 'Termin vereinbart']], 'ungelesen': 1, 'kontaktperson': ['is', 'set']}, limit=100, distinct=True)),
-            'r10_bs': len(frappe.get_list('Beratung', fields='name', filters={'status': ['!=', 'Closed'],'_user_tags': ['like', '%MNE-MVBS%']}, limit=100, distinct=True)),
-            'r11_bs': len(frappe.get_list('Beratung', fields='name', filters={'status': ['!=', 'Closed'],'_user_tags': ['like', '%Mandat-MVBS%']}, limit=100, distinct=True)),
-            'r12_bs': len(frappe.get_list('Beratung', fields='name', filters={'status': ['!=', 'Closed'],'_user_tags': ['like', '%PHF-MVBS%']}, limit=100, distinct=True)),
-            'p1_as': get_p1(frappe.session.user),
-            'p2_as': get_p2(frappe.session.user),
-            'p3_as': get_p3(frappe.session.user),
-            'p4_as': get_p4(frappe.session.user)
+            'datenstand': now_datetime().strftime("%d.%m.%Y %H:%M:%S"),
+            's1': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Eingang', 'mv_mitgliedschaft': ['is', 'not set']}, limit=100, distinct=True)),
+            's6': len(frappe.get_list('Beratung', fields='name', filters={'status': ['not in', ['Rückfragen', 'Rückfrage: Termin vereinbaren', 'Eingang', 'Open', 'Zusammengeführt']], 'ungelesen': 1, 'kontaktperson': ['is', 'not set']}, limit=100, distinct=True)),
+            's10': len(frappe.get_list('Beratung', fields='name', filters={
+                'ungelesen': 1, 
+                'sektion_id': ['!=', 'MVDF'],
+                'name': ['in', frappe.db.sql_list("SELECT DISTINCT parent FROM `tabBeratung Termin` WHERE `von` > %s", now_datetime())]
+            }, limit=100, distinct=True)),
+            'r': len(frappe.get_list('Beratung', fields='name', filters={'status': ['in', ['Open', 'In Arbeit']]}, limit=100, distinct=True)),
+            'r1': len(frappe.get_list('Beratung', fields='name', filters={'status': ['in', ['Open', 'In Arbeit']], 'beratung_prio': 'Hoch'}, limit=100, distinct=True)),
+            'r2': len(frappe.get_list('Beratung', fields='name', filters={'status': ['in', ['Open', 'In Arbeit']], 'beratung_prio': ['not in', ['Hoch']], 'kontaktperson': ['like','Rechtsberatung Pool%']}, limit=100, distinct=True)),
+            'r3': len(frappe.get_list('Beratung', fields='name', filters={'status': ['in', ['Open', 'In Arbeit']], 'r3': 1}, limit=100, distinct=True)),
+            'r4': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['is', 'set'], 'ungelesen': 0}, limit=100, distinct=True)),
+            'r5': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['like', 'Rechtsberatung Pool%'], 'ungelesen': 1}, limit=100, distinct=True)),
+            'r6': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Rückfragen', 'kontaktperson': ['not like', 'Rechtsberatung Pool%'], 'ungelesen': 1}, limit=100, distinct=True)),
+            'r7': len(frappe.get_list('Beratung', fields='name', filters={'status': ['!=', 'Closed'], 'hat_termine': 1}, limit=100, distinct=True)),
+            'r8': len(frappe.get_list('Beratung', fields='name', filters={'status': 'Closed', 'hat_termine': 1}, limit=100, distinct=True)),
+            'r9': len(frappe.get_list('Beratung', fields='name', filters={'status': ['not in', ['Rückfragen', 'Open', 'Zusammengeführt', 'Termin vereinbart', "Rückfrage: Termin vereinbaren"]], 'ungelesen': 1, 'kontaktperson': ['is', 'set']}, limit=100, distinct=True)),
+            'p1': get_p1(frappe.session.user),
+            'p2': get_p2(frappe.session.user),
+            'p3': get_p3(frappe.session.user),
+            'p4': get_p4(frappe.session.user)
         }
     }
     
@@ -51,9 +46,8 @@ def get_p1(user):
                                                 FROM `tabTermin Kontaktperson Multi User`
                                                 WHERE `user` = '{user}'  
                                                 AND `parent` NOT LIKE 'Rechtsberatung Pool (%)'
-                                                AND `parent` NOT LIKE 'Administration (%)'
                                             )
-                                            AND `status` = 'Open'""".format(user=user), as_dict=True)[0].qty
+                                            AND `status` IN ('Open', 'In Arbeit')""".format(user=user), as_dict=True)[0].qty
     return p1_qty or 0
 
 def get_p2(user):
@@ -63,7 +57,6 @@ def get_p2(user):
                                                 FROM `tabTermin Kontaktperson Multi User`
                                                 WHERE `user` = '{user}'  
                                                 AND `parent` NOT LIKE 'Rechtsberatung Pool (%)'
-                                                AND `parent` NOT LIKE 'Administration (%)'
                                             )
                                             AND `status` = 'Rückfragen'
                                             AND `ungelesen` = 0""".format(user=user), as_dict=True)[0].qty
@@ -75,7 +68,6 @@ def get_p3(user):
                                                 SELECT `parent`
                                                 FROM `tabTermin Kontaktperson Multi User`
                                                 WHERE `user` = '{user}'  AND `parent` NOT LIKE 'Rechtsberatung Pool (%)'
-                                                AND `parent` NOT LIKE 'Administration (%)'
                                             )
                                             AND `status` = 'Rückfragen'
                                             AND `ungelesen` = 1""".format(user=user), as_dict=True)[0].qty
@@ -88,18 +80,18 @@ def get_p4(user):
                                                 FROM `tabTermin Kontaktperson Multi User`
                                                 WHERE `user` = '{user}'  
                                                 AND `parent` NOT LIKE 'Rechtsberatung Pool (%)'
-                                                AND `parent` NOT LIKE 'Administration (%)'
                                             )
-                                            AND `status` = 'Termin vereinbart'""".format(user=user), as_dict=True)[0].qty
+                                            AND `status` = 'Termin vereinbart'
+                                            AND `hat_termine` = 1""".format(user=user), as_dict=True)[0].qty
     return p4_qty or 0
+
 
 @frappe.whitelist()
 def get_user_kontaktperson(only_session_user=False):
     user_kontaktperson = frappe.db.sql("""SELECT `parent`
                                         FROM `tabTermin Kontaktperson Multi User`
                                         WHERE `user` = '{user}' 
-                                        AND `parent` NOT LIKE 'Rechtsberatung Pool (%)'
-                                        AND `parent` NOT LIKE 'Administration (%)'""".format(user=frappe.session.user), as_dict=True)
+                                        AND `parent` NOT LIKE 'Rechtsberatung Pool (%)'""".format(user=frappe.session.user), as_dict=True)
     user_kontaktpersonen = []
     for uk in user_kontaktperson:
         if not only_session_user:
@@ -109,3 +101,6 @@ def get_user_kontaktperson(only_session_user=False):
                 user_kontaktpersonen.append(uk.parent)
     return user_kontaktpersonen
                 
+@frappe.whitelist()
+def get_termine_in_zukunft():
+    return frappe.db.sql_list("SELECT DISTINCT parent FROM `tabBeratung Termin` WHERE `von` > %s", now_datetime())
