ffcb279f07c0b0afb8dead670922bb6baed534cb
Joel <joel.mesmer@libracore.com>
2026-08-19 13:58:38 +0200

    #1953

--- mvd/mvd/doctype/siedlung/siedlung.py
+++ mvd/mvd/doctype/siedlung/siedlung.py
@@ -16,3 +16,19 @@ class Siedlung(Document):
                 self.zugehoerige_gebaeude[0].adr_number
             )
             self.save()
+
+    def validate(self):
+        entries = []
+        duplikate = []
+        for zugehoeriges_gebaeude in self.zugehoerige_gebaeude:
+            if zugehoeriges_gebaeude.adr_egaid:
+                if zugehoeriges_gebaeude.adr_egaid not in entries:
+                    entries.append(zugehoeriges_gebaeude.adr_egaid)
+                else:
+                    duplikate.append("Zeile {0} ({1})".format(zugehoeriges_gebaeude.idx, zugehoeriges_gebaeude.adr_egaid))
+        if len(duplikate) > 0:
+            frappe.msgprint(
+                msg="Diese Siedlung enthält doppelte Adress-Einträge, siehe nachfolgende Liste:<br>{0}".format("<br>".join(duplikate)),
+                title="Siedlung enthält doppelte Adress-Einträge",
+                indicator="orange"
+            )
