7d496d0aa37fefe4187f9c0fd6ec3bfaaa85286e
Joel <joel.mesmer@libracore.com>
2026-09-23 15:39:20 +0200

    #2132

--- mvd/mvd/doctype/addresschange/addresschange.js
+++ mvd/mvd/doctype/addresschange/addresschange.js
@@ -2,57 +2,63 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Addresschange', {
-	refresh: function(frm) {
-		if (frm.doc.docstatus === 0 && !frm.is_new()) {
-			frm.set_intro(__("Die Adressänderung ist gespeichert und automatisch am angegebenen Datum (Gültig ab) gebucht/wirksam."), "blue");
-		}
+    setup: function(frm) {
+		frm.show_submit_message = function() {};
 	},
-	plz: function(frm) {
-		pincode_lookup(frm.doc.plz, 'ort');
-	}
+    refresh: function(frm) {
+        if (frm.doc.docstatus === 0 && !frm.is_new()) {
+            frm.set_intro(__(`
+                Die Adressänderung ist gespeichert und wird automatisch am angegebenen Datum (Gültig ab) gebucht/wirksam.<br>
+                Für eine sofortige Ausführung kann die Adressänderung manuell gebucht werden.
+            `), "blue");
+        }
+    },
+    plz: function(frm) {
+        pincode_lookup(frm.doc.plz, 'ort');
+    }
 });
 
 function pincode_lookup(pincode, ort) {
-	var filters = [['pincode','=', pincode]];
-	if (pincode) {
-		frappe.call({
-			method: 'frappe.client.get_list',
-			args: {
-				doctype: 'Pincode',
-				filters: filters,
-				fields: ['name', 'pincode', 'city', 'canton_code']
-			},
-			async: false,
-			callback: function(response) {
-				if (response.message) {
-					if (response.message.length == 1) {
-						var city = response.message[0].city;
-						cur_frm.set_value(ort, city);
-					} else {
-						var cities = "";
-						response.message.forEach(function (record) {
-							cities += (record.city + "\n");
-						});
-						cities = cities.substring(0, cities.length - 1);
-						frappe.prompt([
-								{'fieldname': 'city',
-								 'fieldtype': 'Select',
-								 'label': 'City',
-								 'reqd': 1,
-								 'options': cities,
-								 'default': response.message[0].city
-								}
-							],
-							function(values){
-								var city = values.city;
-								cur_frm.set_value(ort, city);
-							},
-							__('City'),
-							__('Set')
-						);
-					}
-				}
-			}
-		});
-	}
+    var filters = [['pincode','=', pincode]];
+    if (pincode) {
+        frappe.call({
+            method: 'frappe.client.get_list',
+            args: {
+                doctype: 'Pincode',
+                filters: filters,
+                fields: ['name', 'pincode', 'city', 'canton_code']
+            },
+            async: false,
+            callback: function(response) {
+                if (response.message) {
+                    if (response.message.length == 1) {
+                        var city = response.message[0].city;
+                        cur_frm.set_value(ort, city);
+                    } else {
+                        var cities = "";
+                        response.message.forEach(function (record) {
+                            cities += (record.city + "\n");
+                        });
+                        cities = cities.substring(0, cities.length - 1);
+                        frappe.prompt([
+                                {'fieldname': 'city',
+                                    'fieldtype': 'Select',
+                                    'label': 'City',
+                                    'reqd': 1,
+                                    'options': cities,
+                                    'default': response.message[0].city
+                                }
+                            ],
+                            function(values){
+                                var city = values.city;
+                                cur_frm.set_value(ort, city);
+                            },
+                            __('City'),
+                            __('Set')
+                        );
+                    }
+                }
+            }
+        });
+    }
 }
